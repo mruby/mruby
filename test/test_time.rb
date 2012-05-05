9684f83b70dca5fff66a2f09b0a8f087f5172be4
Kazuki Tsujimoto <kazuki@callcc.net>
2012-05-05 15:46:44 +0900

    Add test imported from CRuby

--- test/test_time.rb
+++ test/test_time.rb
@@ -1,48 +1,50 @@
-$ok     = 0
-$failed = 0
-$ftest  = []
+assert_equal %q{2012}, %q{
+  Time.gm(2012, 12, 23).year
+}
+assert_equal %q{12}, %q{
+  Time.gm(2012, 12, 23).month
+}
+assert_equal %q{23}, %q{
+  Time.gm(2012, 12, 23).mday
+}
 
-def assert(str = "Assertion failed")
-    if(!yield)
-      $ftest.push(str)
-      $failed += 1
-      print "F"
-    else
-      $ok     += 1
-      print "."
-    end
-end
-
-def report()
-  print "\n"
-  $ftest.each do |str|
-    puts("Test Failed: #{str}");
+# TODO
+assert_equal %q{ng}, %q{
+  $x = :ng
+  begin
+    Time.new
+  rescue
+    $x = :ok
   end
-  $total = $ok + $failed
-  puts "Ran #{$total} tests: #{$ok} OK and #{$failed} failed."
-end
+  $x
+}
 
-doom = Time.gm(2012, 12, 23)
-assert("Time.gm")   { doom                      }
-assert("gm year")   { doom.year == 2012         }
-assert("gm month")  { doom.month == 12          }
-assert("gm day")    { doom.mday == 23           }
+assert_equal %q{Sun Mar 13 07:06:40 UTC 2011}, %q{
+  Time.at(1300000000.0).utc.asctime
+}
+assert_equal %q{0}, %q{
+  Time.at(1300000000.0).utc.usec
+}
+assert_equal %q{1300000000}, %q{
+  Time.at(1300000000.0).utc.to_i
+}
+assert_equal %q{1300000000.0}, %q{
+  Time.at(1300000000.0).utc.to_f
+}
+assert_equal %q{true}, %q{
+  Time.at(1300000000.0).utc.utc?
+}
+assert_equal %q{UTC}, %q{
+  Time.at(1300000000.0).utc.zone
+}
+assert_equal %q{0}, %q{
+  Time.at(1300000000.0).utc.wday
+}
+assert_equal %q{2011}, %q{
+  Time.at(1300000000.0).utc.year
+}
 
-t0 = Time.new
-assert("Can create time.") { t0 }
-
-t1 = Time.at(1300000000.0).utc
-assert("asctime") { t1.asctime == "Sun Mar 13 07:06:40 UTC 2011" }
-assert("usec")    { t1.usec    ==  0              }
-assert("to_i")    { t1.to_i    ==  1300000000     }
-assert("to_f")    { t1.to_f    ==  1300000000.0   }
-assert("utc?")    { t1.utc?                       }
-assert("zone")    { t1.zone    == "UTC"           }
-assert("wday")    { t1.wday    ==  0              }
-assert("yday")    { t1.yday    ==  71             }
-assert("year")    { t1.year    ==  2011           }
-
-t2 = Time.at(7.0e6)
-assert("initialize_copy") { t2.clone == t2 }
-
-report()
+assert_equal %q{true}, %q{
+  t = Time.at(7.0e6)
+  t.clone == t
+}
