22bd603a7ee41f018c81fad94e6f18738713245b
Yukihiro Matsumoto <matz@ruby-lang.org>
2012-05-09 10:59:05 +0900

    move declaration in the middle

--- include/mruby/khash.h
+++ include/mruby/khash.h
@@ -112,10 +112,10 @@ static const uint8_t __m[8] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80};
       khkey_t *old_keys = h->keys;                                      \
       khval_t *old_vals = h->vals;                                      \
       khint_t old_n_buckets = h->n_buckets;                             \
+      khint_t i;                                                        \
       h->n_buckets = new_n_buckets;                                     \
       kh_alloc_##name(h);                                               \
       /* relocate */                                                    \
-      khint_t i;                                                        \
       for( i=0 ; i<old_n_buckets ; i++ ){                               \
 	if( !__ac_isempty(old_e_flags, old_d_flags, i) ){               \
 	  khint_t k = kh_put_##name(h, old_keys[i]);                    \
