6faab394d721011080355b1d0fa14d925a2a7734
Yukihiro Matsumoto <matz@ruby-lang.org>
2012-05-09 22:11:53 +0900

    remove duplicated document comment

--- src/enum.c
+++ src/enum.c
@@ -6,17 +6,6 @@
 
 #include "mruby.h"
 
-/*
- *  The <code>Enumerable</code> mixin provides collection classes with
- *  several traversal and searching methods, and with the ability to
- *  sort. The class must provide a method <code>each</code>, which
- *  yields successive members of the collection. If
- *  <code>Enumerable#max</code>, <code>#min</code>, or
- *  <code>#sort</code> is used, the objects in the collection must also
- *  implement a meaningful <code><=></code> operator, as these methods
- *  rely on an ordering between members of the collection.
- */
-
 void
 mrb_init_enumerable(mrb_state *mrb)
 {
