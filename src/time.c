25d787286b9a8c7a793803b8f0d33f141fbeaafd
mattn <mattn.jp@gmail.com>
2012-05-16 09:32:30 +0900

    fix #127 on mingw.

--- src/time.c
+++ src/time.c
@@ -27,8 +27,12 @@
 
 #ifdef _WIN32
 /* Win32 platform do not provide gmtime_r/localtime_r; emulate them using gmtime_s/localtime_s */
+#if _MVC_VER
 #define gmtime_r(tp, tm)    ((gmtime_s((tm), (tp)) == 0) ? (tp) : NULL)
 #define localtime_r(tp, tm)    ((localtime_s((tm), (tp)) == 0) ? (tp) : NULL)
+#else
+#define NO_GMTIME_R
+#endif
 #endif
 
 /* timegm(3) */
