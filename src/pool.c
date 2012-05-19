59da9f6974d1f239bc783dcd2cb0640ced6d0030
Yukihiro Matsumoto <matz@ruby-lang.org>
2012-05-19 16:56:15 +0900

    remove src/pool.h; close #163

--- src/pool.c
+++ src/pool.c
@@ -4,8 +4,20 @@
 ** See Copyright Notice in mruby.h
 */
 
-#include "pool.h"
+#include "mruby.h"
+#include <stddef.h>
 #include <string.h>
+
+struct mrb_pool {
+  mrb_state *mrb;
+  struct mrb_pool_page {
+    struct mrb_pool_page *next;
+    size_t offset;
+    size_t len;
+    void *last;
+    char page[1];
+  } *pages;
+};
 
 #undef TEST_POOL
 #ifdef TEST_POOL
