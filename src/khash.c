8b8151a60e53e3182382acca8b128c6d1d7f0b4f
Yukihiro Matsumoto <matz@ruby-lang.org>
2012-06-21 10:46:21 +0900

    remove khash.c; khash.h dependency to mruby.h; refactoring

--- src/khash.c
+++
@@ -1,5 +0,0 @@
-#include "mruby/khash.h"
-
-KHASH_DEFINE(mt, mrb_sym,   struct RProc*, 1, kh_int_hash_func, kh_int_hash_equal)
-KHASH_DEFINE(iv, mrb_sym,   mrb_value,     1, kh_int_hash_func, kh_int_hash_equal)
-
