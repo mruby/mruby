50f56a2436154211a383d34828efa1db2073aeed
Yukihiro Matsumoto <matz@ruby-lang.org>
2012-07-15 16:22:54 +0900

    Array#join to accept only strings as sep

--- src/array.c
+++ src/array.c
@@ -1006,7 +1006,7 @@ mrb_ary_join(mrb_state *mrb, mrb_value ary, mrb_value sep)
 
 /*
  *  call-seq:
- *     ary.join(sep=nil)    -> str
+ *     ary.join(sep="")    -> str
  *
  *  Returns a string created by converting each element of the array to
  *  a string, separated by <i>sep</i>.
@@ -1020,7 +1020,7 @@ mrb_ary_join_m(mrb_state *mrb, mrb_value ary)
 {
   mrb_value sep = mrb_nil_value();
 
-  mrb_get_args(mrb, "|o", &sep);
+  mrb_get_args(mrb, "|S", &sep);
   return mrb_ary_join(mrb, ary, sep);
 }
 
