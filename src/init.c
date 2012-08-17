a190ec8298f82cf007fa8b68788dfd5064413fa4
Selman ULUG <selman.ulug@gmail.com>
2012-08-17 16:48:07 +0300

    function prototypes sorted by call & removed unused (file, io, thread)

--- src/init.c
+++ src/init.c
@@ -6,30 +6,28 @@
 
 #include "mruby.h"
 
-void mrb_init_class(mrb_state*);
 void mrb_init_symtbl(mrb_state*);
-void mrb_init_symbol(mrb_state*);
+void mrb_init_class(mrb_state*);
 void mrb_init_object(mrb_state*);
 void mrb_init_kernel(mrb_state*);
-void mrb_init_enumerable(mrb_state*);
 void mrb_init_comparable(mrb_state*);
+void mrb_init_enumerable(mrb_state*);
+void mrb_init_symbol(mrb_state*);
+void mrb_init_exception(mrb_state*);
+void mrb_init_proc(mrb_state*);
+void mrb_init_string(mrb_state*);
 void mrb_init_array(mrb_state*);
 void mrb_init_hash(mrb_state*);
 void mrb_init_numeric(mrb_state*);
-void mrb_init_proc(mrb_state*);
 void mrb_init_range(mrb_state*);
-void mrb_init_string(mrb_state*);
-void mrb_init_regexp(mrb_state*);
-void mrb_init_exception(mrb_state*);
-void mrb_init_time(mrb_state*);
-void mrb_init_io(mrb_state*);
-void mrb_init_file(mrb_state*);
-void mrb_init_thread(mrb_state*);
 void mrb_init_struct(mrb_state*);
 void mrb_init_gc(mrb_state*);
+void mrb_init_regexp(mrb_state*);
 void mrb_init_print(mrb_state*);
-void mrb_init_mrblib(mrb_state*);
+void mrb_init_time(mrb_state*);
 void mrb_init_math(mrb_state*);
+void mrb_init_mrblib(mrb_state*);
+
 
 void
 mrb_init_core(mrb_state *mrb)
