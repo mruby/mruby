bf7f9f83c72bb5baf0fe67c3b411239b17fa9eb5
Yukihiro Matsumoto <matz@ruby-lang.org>
2012-09-04 17:38:27 +0900

    rename config macro name IV_INITIAL_SIZE -> IVHASH_INIT_SIZE

--- src/variable.c
+++ src/variable.c
@@ -251,8 +251,8 @@ iv_free(mrb_state *mrb, iv_tbl *t)
 
 #include "mruby/khash.h"
 
-#ifndef MRB_IV_INITIAL_SIZE
-#define MRB_IV_INITIAL_SIZE 8
+#ifndef MRB_IVHASH_INIT_SIZE
+#define MRB_IVHASH_INIT_SIZE 8
 #endif
 
 KHASH_DECLARE(iv, mrb_sym, mrb_value, 1)
@@ -265,7 +265,7 @@ typedef struct iv_tbl {
 static iv_tbl*
 iv_new(mrb_state *mrb)
 {
-  return (iv_tbl*)kh_init_size(iv, mrb, MRB_IV_INITIAL_SIZE);
+  return (iv_tbl*)kh_init_size(iv, mrb, MRB_IVHASH_INIT_SIZE);
 }
 
 static void
