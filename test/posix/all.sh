7e9a14e757872c853e93e8b3310314707c55d717
Akira Yumiyama <yumiyama.akira@gmail.com>
2012-10-07 09:40:30 +0900

    update test/posix/all.sh

--- test/posix/all.sh
+++ test/posix/all.sh
@@ -2,6 +2,7 @@
 
 D=`dirname $0`
 
+result=""
 retval=0
 list=`cd ${D}; ls -1 *.sh | grep -v all.sh`
 
@@ -9,16 +10,22 @@ for t in $list; do
 	echo "###################################"
 	echo "# exec test/posix/${t}; start..."
 	sh -c "cd $D; sh $t"
-	retval=`expr $retval + $?`
+	ret=$?
+	retval=`expr $retval + $ret`
+	if [ $ret -ne 0 ]; then
+		result+="  ${t}"
+	fi
 	echo
 done
 
-
+echo "###################################"
 if [ $retval -eq 0 ]; then
 	echo "POSIX test result: OK"
 else
 	echo "POSIX test result: NG"
+	echo "  fail test is :${result}"
 fi
+echo "###################################"
 
 exit $retval
 
