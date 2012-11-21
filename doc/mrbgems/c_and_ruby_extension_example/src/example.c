83fdc3d78e59e1c87ba8ba75445b65ff2f146ae3
Daniel Bovensiepen <daniel@bovensiepen.net>
2012-11-21 15:54:54 +0800

    Add C and Ruby Extension Example Mixed

---
+++ doc/mrbgems/c_and_ruby_extension_example/src/example.c
@@ -0,0 +1,17 @@
+#include <mruby.h>
+#include <stdio.h>
+
+static struct RClass *_class_cextension;
+
+static mrb_value
+mrb_c_method(mrb_state *mrb, mrb_value self)
+{
+  puts("A C Extension");
+  return self;
+}
+
+void
+mrb_c_and_ruby_extension_example_gem_init(mrb_state* mrb) {
+  _class_cextension = mrb_define_module(mrb, "CRubyExtension");
+  mrb_define_class_method(mrb, _class_cextension, "c_method", mrb_c_method, ARGS_NONE());
+}
