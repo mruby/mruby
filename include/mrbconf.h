bc2e7c736f41364694a89a3d69c6c30810588816
Cremno <cremno@mail.ru>
2012-12-16 07:18:25 +0100

    MSVC doesn't define strtoll but _strtoi64.

--- include/mrbconf.h
+++ include/mrbconf.h
@@ -127,6 +127,7 @@ typedef short mrb_sym;
 # define snprintf _snprintf
 # define isnan _isnan
 # define isinf(n) (!_finite(n) && !_isnan(n))
+# define strtoll _strtoi64
 #endif
 
 #endif  /* MRUBYCONF_H */
