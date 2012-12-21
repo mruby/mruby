dee0b2283c180c4e2584ff27dcb241a80fb7cfc2
Yuichiro MASUI <masui@masuidrive.jp>
2012-12-22 01:38:06 +0900

    Add MRUBY_CFLAGS / MRUBY_LDFLAGS to mrbgems

--- test/init_mrbtest.c
+++ test/init_mrbtest.c
@@ -5,14 +5,15 @@
 #include "mruby/proc.h"
 
 extern const char mrbtest_irep[];
-extern const char mrbgemtest_irep[];
+
+void mrbgemtest_init(mrb_state* mrb);
 
 void
 mrb_init_mrbtest(mrb_state *mrb)
 {
   mrb_load_irep(mrb, mrbtest_irep);
 #ifndef DISABLE_GEMS
-  mrb_load_irep(mrb, mrbgemtest_irep);
+  mrbgemtest_init(mrb);
 #endif
   if (mrb->exc) {
     mrb_p(mrb, mrb_obj_value(mrb->exc));
