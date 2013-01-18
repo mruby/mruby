mruby/mruby

commit 79d38c496266faccc9823b396a103c892ee11259

check if array shrinks in each block

mrblib/array.rb
@@ -12,7 +12,13 @@ class Array
   def each(&block)
     idx, length = -1, self.length-1
     while(idx < length)
-      block.call(self[idx += 1])
+      elm = self[idx += 1]
+      unless elm
+        if elm == nil and length >= self.length
+          break
+        end
+      end
+      block.call(elm)
     end
     self
   end

commit c9c758d6ede1f99fa5c4bd20b26f4bce0b88c84e

fixed wrong loop termination introduced by 79d38c4

mrblib/array.rb
@@ -11,7 +11,7 @@ class Array
   # ISO 15.2.12.5.10
   def each(&block)
     idx, length = -1, self.length-1
-    while idx < length and length < self.length and length = self.length-1
+    while idx < length and length <= self.length and length = self.length-1
       elm = self[idx += 1]
       unless elm
         if elm == nil and length >= self.length