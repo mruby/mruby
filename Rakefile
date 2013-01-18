mruby/mruby

commit 33350251ae3d8d683ac4b4c462e7f8aa690c25da

Added conf.bins for defining bulding binaries

Rakefile
@@ -27,25 +27,21 @@ load 'test/mrbtest.rake'
 # generic build targets, rules
 task :default => :all
 
-binfiles = [exefile('bin/mruby'), exefile('bin/mirb'), exefile('bin/mrbc')]
+binfiles = MRuby.targets['host'].bins.map do |bin|
+  install_path = exefile("bin/#{bin}")
+  
+  file install_path => exefile("build/host/bin/#{bin}") do |t|
+    FileUtils.cp t.prerequisites.first, t.name
+  end
+
+  install_path
+end
 
 desc "build all targets, install (locally) in-repo"
-task :all => binfiles + MRuby.targets.map { |t| [exefile("#{t.build_dir}/bin/mruby"), exefile("#{t.build_dir}/bin/mirb"), exefile("#{t.build_dir}/bin/mrbc")] }.flatten
-
-file exefile('bin/mruby') => exefile('build/host/bin/mruby') do |t|
-  FileUtils.cp t.prerequisites.first, t.name
-end
-
-file exefile('bin/mirb') => exefile('build/host/bin/mirb') do |t|
-  FileUtils.cp t.prerequisites.first, t.name
-end
-
-file exefile('bin/mrbc') => exefile('build/host/bin/mrbc') do |t|
-  FileUtils.cp t.prerequisites.first, t.name
-end
+task :all => binfiles + MRuby.targets.values.map { |t| [exefile("#{t.build_dir}/bin/mruby"), exefile("#{t.build_dir}/bin/mirb"), exefile("#{t.build_dir}/bin/mrbc")] }.flatten
 
 desc "run all mruby tests"
-task :test => MRuby.targets.map { |t| exefile("#{t.build_dir}/test/mrbtest") } do
+task :test => MRuby.targets.values.map { |t| exefile("#{t.build_dir}/test/mrbtest") } do
   sh "#{filename exefile('build/host/test/mrbtest')}"
   if MRuby.targets.count > 1
     puts "\nYou should run #{MRuby.targets.map{ |t| t.name == 'host' ? nil : "#{t.build_dir}/test/mrbtest" }.compact.join(', ')} on target device."
@@ -54,7 +50,7 @@ end
 
 desc "clean all built and in-repo installed artifacts"
 task :clean do
-  MRuby.targets.each do |t|
+  MRuby.each_target do |t|
     FileUtils.rm_rf t.build_dir
   end
   FileUtils.rm_f binfiles

build_config.rb
@@ -2,6 +2,7 @@ MRuby::Build.new do |conf|
   conf.cc = ENV['CC'] || 'gcc'
   conf.ld = ENV['LD'] || 'gcc'
   conf.ar = ENV['AR'] || 'ar'
+  # conf.bins = %w(mrbc mruby mirb)
   # conf.cxx = conf.cc
   # conf.objcc = conf.cc
   # conf.asm = conf.cc
@@ -27,6 +28,7 @@ MRuby::CrossBuild.new('i386') do |conf|
   conf.cc = ENV['CC'] || 'gcc'
   conf.ld = ENV['LD'] || 'gcc'
   conf.ar = ENV['AR'] || 'ar'
+  # conf.bins = %w(mrbc mruby mirb)
   # conf.cxx = 'gcc'
   # conf.objcc = 'gcc'
   # conf.asm = 'gcc'

tasks/mruby_build.rake
@@ -3,11 +3,11 @@ module MRuby
     attr_accessor :build
 
     def targets
-      @targets ||= []
+      @targets ||= {}
     end
 
     def each_target(&block)
-      @targets.each do |target|
+      @targets.each do |key, target|
         target.instance_eval(&block)
       end
     end
@@ -22,6 +22,7 @@ module MRuby
     attr_writer :cxx, :cxxflags
     attr_writer :objcc, :objcflags
     attr_writer :asm, :asmflags
+    attr_accessor :bins
     attr_accessor :gperf, :yacc
     attr_accessor :cat, :git
     attr_reader :root, :gems
@@ -38,9 +39,11 @@ module MRuby
       @yacc, @gperf = 'bison', 'gperf'
       @cat, @git = 'cat', 'git'
 
+      @bins = %w(mruby mrbc mirb)
+
       @gems, @libmruby = [], []
 
-      MRuby.targets << self
+      MRuby.targets[name.to_s] = self
       MRuby.build = self
       instance_eval(&block)
     end

tools/mirb/mirb.rake
@@ -1,10 +1,12 @@
 dir = File.dirname(__FILE__).sub(%r|^\./|, '')
 
 MRuby.each_target do
-  exec = exefile("#{build_dir}/bin/mirb")
-  objs = Dir.glob("#{dir}/*.{c}").map { |f| f.pathmap("#{build_dir}/%X.o") }
+  if bins.select { |s| s.to_s == 'mirb' }
+    exec = exefile("#{build_dir}/bin/mirb")
+    objs = Dir.glob("#{dir}/*.{c}").map { |f| f.pathmap("#{build_dir}/%X.o") }
 
-  file exec => objs + ["#{build_dir}/lib/libmruby.a"] do |t|
-    link t.name, t.prerequisites, gems.map { |g| g.mruby_ldflags }, gems.map { |g| g.mruby_libs }
+    file exec => objs + ["#{build_dir}/lib/libmruby.a"] do |t|
+      link t.name, t.prerequisites, gems.map { |g| g.mruby_ldflags }, gems.map { |g| g.mruby_libs }
+    end
   end
 end

tools/mrbc/mrbc.rake
@@ -1,10 +1,12 @@
 dir = File.dirname(__FILE__).sub(%r|^\./|, '')
 
 MRuby.each_target do
-  exec = exefile("#{build_dir}/bin/mrbc")
-  objs = Dir.glob("#{dir}/*.{c}").map { |f| f.pathmap("#{build_dir}/%X.o") }
+  if bins.select { |s| s.to_s == 'mrbc' }
+    exec = exefile("#{build_dir}/bin/mrbc")
+    objs = Dir.glob("#{dir}/*.{c}").map { |f| f.pathmap("#{build_dir}/%X.o") }
 
-  file exec => objs + ["#{build_dir}/lib/libmruby_core.a"] do |t|
-    link t.name, t.prerequisites, [], gems.map { |g| g.mruby_libs }
+    file exec => objs + ["#{build_dir}/lib/libmruby_core.a"] do |t|
+      link t.name, t.prerequisites, [], gems.map { |g| g.mruby_libs }
+    end
   end
 end

tools/mruby/mruby.rake
@@ -1,10 +1,12 @@
 dir = File.dirname(__FILE__).sub(%r|^\./|, '')
 
 MRuby.each_target do
-  exec = exefile("#{build_dir}/bin/mruby")
-  objs = Dir.glob("#{dir}/*.{c}").map { |f| f.pathmap("#{build_dir}/%X.o") }
+  if bins.select { |s| s.to_s == 'mruby' }
+    exec = exefile("#{build_dir}/bin/mruby")
+    objs = Dir.glob("#{dir}/*.{c}").map { |f| f.pathmap("#{build_dir}/%X.o") }
 
-  file exec => objs + ["#{build_dir}/lib/libmruby.a"] do |t|
-    link t.name, t.prerequisites, gems.map { |g| g.mruby_ldflags }, gems.map { |g| g.mruby_libs }
+    file exec => objs + ["#{build_dir}/lib/libmruby.a"] do |t|
+      link t.name, t.prerequisites, gems.map { |g| g.mruby_ldflags }, gems.map { |g| g.mruby_libs }
+    end
   end
 end

commit 5c14e9a41b77f42e694e9ef241932ab4223b0655

Fix an error on rake in case cross builds. It may be caused by 33350251ae3.

Rakefile
@@ -49,7 +49,7 @@ desc "run all mruby tests"
 task :test => MRuby.targets.values.map { |t| exefile("#{t.build_dir}/test/mrbtest") } do
   sh "#{filename exefile('build/host/test/mrbtest')}"
   if MRuby.targets.count > 1
-    puts "\nYou should run #{MRuby.targets.map{ |t| t.name == 'host' ? nil : "#{t.build_dir}/test/mrbtest" }.compact.join(', ')} on target device."
+    puts "\nYou should run #{MRuby.targets.values.map{ |t| t.name == 'host' ? nil : "#{t.build_dir}/test/mrbtest" }.compact.join(', ')} on target device."
   end
 end
 