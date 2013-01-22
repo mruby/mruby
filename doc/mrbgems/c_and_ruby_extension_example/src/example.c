310b0dce8fa1e3f98b2184c2b6c1a0f091c2d4eb
Yuichiro MASUI <masui@masuidrive.jp>
2013-01-22 20:18:48 +0900

    Added finalizer of mrbgems

--- doc/mrbgems/c_and_ruby_extension_example/src/example.c
+++ doc/mrbgems/c_and_ruby_extension_example/src/example.c
@@ -13,3 +13,8 @@ mrb_c_and_ruby_extension_example_gem_init(mrb_state* mrb) {
   struct RClass *class_cextension = mrb_define_module(mrb, "CRubyExtension");
   mrb_define_class_method(mrb, class_cextension, "c_method", mrb_c_method, ARGS_NONE());
 }
+
+void
+mrb_c_and_ruby_extension_example_gem_final(mrb_state* mrb) {
+  // finalizer
+}
--- src/init.c
+++ src/init.c
@@ -28,6 +28,7 @@ void mrb_init_time(mrb_state*);
 void mrb_init_math(mrb_state*);
 void mrb_init_mrblib(mrb_state*);
 void mrb_init_mrbgems(mrb_state*);
+void mrb_final_mrbgems(mrb_state*);
 
 #define DONE mrb_gc_arena_restore(mrb, 0);
 void
@@ -68,5 +69,13 @@ mrb_init_core(mrb_state *mrb)
   mrb_init_mrblib(mrb); DONE;
 #ifndef DISABLE_GEMS
   mrb_init_mrbgems(mrb); DONE;
+#endif
+}
+
+void
+mrb_final_core(mrb_state *mrb)
+{
+#ifndef DISABLE_GEMS
+  mrb_final_mrbgems(mrb); DONE;
 #endif
 }
