f2dbf2c039f539ca706b3d1057fb84c4ea3d26a4
skandhas <skandhas@163.com>
2013-03-07 13:18:32 +0800

    add mrbgems\ext\mruby-sring

---
+++ mrbgems/ext/mruby-string/test/string.rb
@@ -0,0 +1,15 @@
+##
+# String(Ext) Test
+
+assert('String#getbyte') do
+  str1 = "hello"
+  bytes1 = [104, 101, 108, 108, 111]
+  assert_equal bytes1[0], str1.getbyte(0)
+  assert_equal bytes1[-1], str1.getbyte(-1)
+  assert_equal bytes1[6], str1.getbyte(6)
+
+  str2 = "\xFF"
+  bytes2 = [0xFF]
+  assert_equal bytes2[0], str2.getbyte(0)
+end
+
