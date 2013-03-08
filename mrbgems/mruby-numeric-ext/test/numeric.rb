d3ff90f5fc99e06433ccb6213461b906101cb623
Kouki Ooyatsu <kouki-o@iij.ad.jp>
2013-03-08 10:41:38 +0900

    fix comment

--- mrbgems/mruby-numeric-ext/test/numeric.rb
+++ mrbgems/mruby-numeric-ext/test/numeric.rb
@@ -5,6 +5,6 @@ assert('Integer#chr') do
   assert_equal(65.chr, "A")
   assert_equal(0x42.chr, "B")
 
-  # encoding multibyte (not support yet)
+  # multibyte encoding (not support yet)
   assert_raise(RangeError) { 12345.chr }
 end
