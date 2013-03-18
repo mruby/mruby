2a341753f888d411851e4c30ecd0f2245c275633
Masaki Muranaka <monaka@monami-ya.jp>
2013-03-19 01:53:15 +0900

    Make array.c and vm.c share value_move().

--- src/vm.c
+++ src/vm.c
@@ -16,6 +16,7 @@
 #include "mruby/class.h"
 #include "mruby/numeric.h"
 #include "error.h"
+#include "value_array.h"
 
 #include <string.h>
 #include <setjmp.h>
@@ -53,27 +54,6 @@ The value below allows about 60000 recursive calls in the simplest case. */
 #else
 # define DEBUG(x)
 #endif
-
-static inline void
-value_move(mrb_value *s1, const mrb_value *s2, size_t n)
-{
-  if (s1 > s2 && s1 < s2 + n)
-  {
-    s1 += n;
-    s2 += n;
-    while (n-- > 0) {
-      *--s1 = *--s2;
-    }
-  }
-  else if (s1 != s2) {
-    while (n-- > 0) {
-      *s1++ = *s2++;
-    }
-  }
-  else {
-    /* nothing to do. */
-  }
-}
 
 static inline void
 stack_clear(mrb_value *from, size_t count)
