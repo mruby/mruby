c1025264349c54b377ecba9e82c8a0a3c166faae
Masaki Muranaka <monaka@monami-ya.jp>
2013-03-19 10:33:34 +0900

    Use mrb_true_or_false_value() / in gc_enable().

--- src/gc.c
+++ src/gc.c
@@ -982,8 +982,8 @@ gc_enable(mrb_state *mrb, mrb_value obj)
   int old = mrb->gc_disabled;
 
   mrb->gc_disabled = FALSE;
-  if (old) return mrb_true_value();
-  return mrb_false_value();
+
+  return mrb_true_or_false_value(old);
 }
 
 /*
