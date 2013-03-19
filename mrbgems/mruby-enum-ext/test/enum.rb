854c61972f563a9f329afc7f71027e7c1572171c
skandhas <skandhas@163.com>
2013-03-19 14:25:03 +0800

    fix spelling

--- mrbgems/mruby-enum-ext/test/enum.rb
+++ mrbgems/mruby-enum-ext/test/enum.rb
@@ -1,24 +1,24 @@
 ##
 # Enumerable(Ext) Test
 
-assert("Enumrable#drop") do
+assert("Enumerable#drop") do
   a = [1, 2, 3, 4, 5, 0]
 
   assert_equal a.drop(3), [4, 5, 0]
   assert_equal a.drop(6), []
 end
 
-assert("Enumrable#drop_while") do
+assert("Enumerable#drop_while") do
   a = [1, 2, 3, 4, 5, 0]
   assert_equal a.drop_while {|i| i < 3 }, [3, 4, 5, 0]
 end
 
-assert("Enumrable#take") do
+assert("Enumerable#take") do
   a = [1, 2, 3, 4, 5, 0]
   assert_equal a.take(3), [1, 2, 3]
 end
 
-assert("Enumrable#take_while") do
+assert("Enumerable#take_while") do
   a = [1, 2, 3, 4, 5, 0]
   assert_equal a.take_while {|i| i < 3 }, [1, 2]
 end
