1756e8fc839fb734bf107cd1aa23fb8a39142642
Masaki Muranaka <monaka@monami-ya.jp>
2013-03-22 17:50:01 +0900

    Add brances to macro parameter. It may be refactored as GET_UNPACK_{b,c} are used only by GETARG_{b,c}.

--- src/opcode.h
+++ src/opcode.h
@@ -24,8 +24,8 @@
 #define GETARG_Bx(i)  ((int)((((mrb_code)(i)) >>  7) & 0xffff))
 #define GETARG_sBx(i) ((int)(GETARG_Bx(i)-MAXARG_sBx))
 #define GETARG_Ax(i)  ((int32_t)((((mrb_code)(i)) >>  7) & 0x1ffffff))
-#define GETARG_UNPACK_b(i,n1,n2) ((int)((((mrb_code)(i)) >> (7+n2)) & (((1<<n1)-1))))
-#define GETARG_UNPACK_c(i,n1,n2) ((int)((((mrb_code)(i)) >> 7) & (((1<<n2)-1))))
+#define GETARG_UNPACK_b(i,n1,n2) ((int)((((mrb_code)(i)) >> (7+(n2))) & (((1<<(n1))-1))))
+#define GETARG_UNPACK_c(i,n1,n2) ((int)((((mrb_code)(i)) >> 7) & (((1<<(n2))-1))))
 #define GETARG_b(i)   GETARG_UNPACK_b(i,14,2)
 #define GETARG_c(i)   GETARG_UNPACK_c(i,14,2)
 
