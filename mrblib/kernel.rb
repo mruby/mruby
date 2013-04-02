cc40f93ac8e9bb93ae0a7cf4893c9fd9f3926d21
Akira Yumiyama <yumiyama@iij.ad.jp>
2013-04-02 15:51:36 +0900

    Kernel#open block support

--- mrblib/kernel.rb
+++ mrblib/kernel.rb
@@ -1,11 +1,11 @@
 module Kernel
-  def open(file, *rest)
+  def open(file, *rest, &block)
     raise ArgumentError unless file.is_a?(String)
 
     if file[0] == "|"
-      IO.popen(file[1..-1], *rest)
+      IO.popen(file[1..-1], *rest, &block)
     else
-      File.open(file, *rest)
+      File.open(file, *rest, &block)
     end
   end
 end
