001ce2612b96d489799962c725586be0f3c8b53f
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2013-04-05 04:40:36 +0900

    method function should return mrb_value

--- src/class.c
+++ src/class.c
@@ -1751,6 +1751,7 @@ static mrb_value
 mrb_mod_s_constants(mrb_state *mrb, mrb_value mod)
 {
   mrb_raise(mrb, E_NOTIMP_ERROR, "Module.constants not implemented");
+  return mrb_nil_value();       /* not reached */
 }
 
 static mrb_value
