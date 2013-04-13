64e5c26aa4393db19ae8a264b4cff46c9b579719
h2so5 <mail@h2so5.net>
2013-04-13 23:27:30 +0900

    Add range_class to mrb_state

--- include/mruby.h
+++ include/mruby.h
@@ -98,6 +98,7 @@ typedef struct mrb_state {
   struct RClass *string_class;
   struct RClass *array_class;
   struct RClass *hash_class;
+  struct RClass *range_class;
 
   struct RClass *float_class;
   struct RClass *fixnum_class;
