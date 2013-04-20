9733dda13f184e4be9ac9f94fc465dae2fbfdb90
Masaki Muranaka <monaka@monami-ya.jp>
2013-04-20 15:14:10 +0900

    Move some macros into src/proc.c. Don't export macros which don't have MRB_ prefix.

--- src/proc.c
+++ src/proc.c
@@ -9,6 +9,15 @@
 #include "mruby/proc.h"
 #include "opcode.h"
 
+/* aspec access */
+#define ARGS_GETREQ(a)          (((a) >> 19) & 0x1f)
+#define ARGS_GETOPT(a)          (((a) >> 14) & 0x1f)
+#define ARGS_GETREST(a)         ((a) & (1<<13))
+#define ARGS_GETPOST(a)         (((a) >> 8) & 0x1f)
+#define ARGS_GETKEY(a)          (((a) >> 3) & 0x1f))
+#define ARGS_GETKDICT(a)        ((a) & (1<<2))
+#define ARGS_GETBLOCK(a)        ((a) & (1<<1))
+
 static mrb_code call_iseq[] = {
   MKOP_A(OP_CALL, 0),
 };
