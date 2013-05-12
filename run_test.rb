e277079616bbc54f70bbf0a312a237f91a3d4f35
Akira Yumiyama <yumiyama.akira@gmail.com>
2013-05-12 14:50:31 +0900

    update run_test.rb

--- run_test.rb
+++ run_test.rb
@@ -3,8 +3,6 @@
 # mrbgems test runner
 #
 
-gemname = File.basename(File.dirname(File.expand_path __FILE__))
-
 if __FILE__ == $0
   repository, dir = 'https://github.com/mruby/mruby.git', 'tmp/mruby'
   build_args = ARGV
@@ -21,6 +19,7 @@ MRuby::Build.new do |conf|
   toolchain :gcc
   conf.gembox 'default'
 
-  conf.gem :github => 'iij/mruby-env'
+  conf.gem :git => 'https://github.com/iij/mruby-env.git'
+
   conf.gem File.expand_path(File.dirname(__FILE__))
 end
