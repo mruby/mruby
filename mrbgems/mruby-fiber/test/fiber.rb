aa2040fc97011b2ea00c0b269a1079f5feb80d01
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2013-05-23 15:24:55 +0900

    add fiber tests

---
+++ mrbgems/mruby-fiber/test/fiber.rb
@@ -0,0 +1,29 @@
+assert('Fiber.new') {
+  f = Fiber.new{}
+  f.class == Fiber
+}
+
+assert('Fiber#resume') {
+  f = Fiber.new{|x| x == 2}
+  f.resume(2)
+}
+
+assert('Fiber.yield') {
+  f = Fiber.new{|x| Fiber.yield(x == 3)}
+  f.resume(3)
+}
+
+assert('Fiber iteration') {
+  f1 = Fiber.new{
+    [1,2,3].each{|x| Fiber.yield(x)}
+  }
+  f2 = Fiber.new{
+    [9,8,7].each{|x| Fiber.yield(x)}
+  }
+  a = []
+  3.times {
+    a << f1.resume
+    a << f2.resume
+  }
+  a == [1,9,2,8,3,7]
+}
