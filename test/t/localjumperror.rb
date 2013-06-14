b825c539987ab8500c260e3fb5cfdac75f922bef
Daniel Bovensiepen <daniel@bovensiepen.net>
2013-06-14 22:17:03 +0800

    Improve LocalJumpError Tests

--- test/t/localjumperror.rb
+++ test/t/localjumperror.rb
@@ -1,13 +1,12 @@
 ##
 # LocalJumpError ISO Test
 
-assert('LocalJumoError', '15.2.25') do
-  begin
+assert('LocalJumpError', '15.2.25') do
+  assert_equal LocalJumpError.class, Class
+  assert_raise LocalJumpError do
     # this will cause an exception due to the wrong location
     retry
-  rescue => e1
   end
-  LocalJumpError.class == Class and e1.class == LocalJumpError
 end
 
 # TODO 15.2.25.2.1 LocalJumpError#exit_value
