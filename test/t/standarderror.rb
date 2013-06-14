7df35e930aac9fffcecd7791c21e3ddeed4ea90b
Daniel Bovensiepen <daniel@bovensiepen.net>
2013-06-14 22:18:52 +0800

    Improve StandardError Tests

--- test/t/standarderror.rb
+++ test/t/standarderror.rb
@@ -2,10 +2,9 @@
 # StandardError ISO Test
 
 assert('StandardError', '15.2.23') do
-  StandardError.class == Class
+  assert_equal StandardError.class, Class
 end
 
 assert('StandardError superclass', '15.2.23.2') do
-  StandardError.superclass == Exception
+  assert_equal StandardError.superclass, Exception
 end
-
