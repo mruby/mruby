f4e169cbfb6a659da5593b274bc2337fcf3f87f7
Daniel Bovensiepen <daniel@bovensiepen.net>
2013-06-14 22:19:10 +0800

    Improve true Tests

--- test/t/true.rb
+++ test/t/true.rb
@@ -2,29 +2,32 @@
 # TrueClass ISO Test
 
 assert('TrueClass', '15.2.5') do
-  TrueClass.class == Class
+  assert_equal TrueClass.class, Class
 end
 
 assert('TrueClass superclass', '15.2.5.2') do
-  TrueClass.superclass == Object
+  assert_equal TrueClass.superclass, Object
 end
 
 assert('TrueClass true', '15.2.5.1') do
-  true
+  assert_true true
 end
 
 assert('TrueClass#&', '15.2.5.3.1') do
-  true.&(true) and not true.&(false)
+  assert_true true.&(true)
+  assert_false true.&(false)
 end
 
 assert('TrueClass#^', '15.2.5.3.2') do
-  not true.^(true) and true.^(false)
+  assert_false true.^(true)
+  assert_true true.^(false)
 end
 
 assert('TrueClass#to_s', '15.2.5.3.3') do
-  true.to_s == 'true'
+  assert_equal true.to_s, 'true'
 end
 
 assert('TrueClass#|', '15.2.5.3.4') do
-  true.|(true) and true.|(false)
+  assert_true true.|(true)
+  assert_true true.|(false)
 end
