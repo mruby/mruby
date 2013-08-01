0b4bedeb8edc5bef55f908cde76f67edf84abf5f
Jun Hiroe <Jun.Hiroe@gmail.com>
2013-08-01 23:00:49 +0900

    I fix order of actual and expect test value in enumerable.rb.

--- test/t/enumerable.rb
+++ test/t/enumerable.rb
@@ -2,7 +2,7 @@
 # Enumerable ISO Test
 
 assert('Enumerable', '15.3.2') do
-  assert_equal(Enumerable.class, Module)
+  assert_equal(Module, Enumerable.class)
 end
 
 assert('Enumerable#all?', '15.3.2.2.1') do
@@ -21,7 +21,7 @@ end
 
 assert('Enumerable#detect', '15.3.2.2.4') do
   assert_true [1,2,3].detect() { true }
-  assert_equal [1,2,3].detect("a") { false }, 'a'
+  assert_equal 'a', [1,2,3].detect("a") { false }
 end
 
 assert('Array#each_with_index', '15.3.2.2.5') do
@@ -30,17 +30,17 @@ assert('Array#each_with_index', '15.3.2.2.5') do
 
   [1].each_with_index {|e,i| a = e; b = i}
 
-  assert_equal(a, 1)
-  assert_equal(b, 0)
+  assert_equal(1, a)
+  assert_equal(0, b)
 end
 
 assert('Enumerable#entries', '15.3.2.2.6') do
-  assert_equal([1].entries, [1])
+  assert_equal([1], [1].entries)
 end
 
 assert('Enumerable#find', '15.3.2.2.7') do
   assert_true [1,2,3].find() { true }
-  assert_equal [1,2,3].find("a") { false }, 'a'
+  assert_equal 'a', [1,2,3].find("a") { false }
 end
 
 assert('Enumerable#find_all', '15.3.2.2.8') do
@@ -48,7 +48,7 @@ assert('Enumerable#find_all', '15.3.2.2.8') do
 end
 
 assert('Enumerable#grep', '15.3.2.2.9') do
-  assert_equal [1,2,3,4,5,6,7,8,9].grep(4..6), [4,5,6]
+  assert_equal [4,5,6], [1,2,3,4,5,6,7,8,9].grep(4..6)
 end
 
 assert('Enumerable#include?', '15.3.2.2.10') do
@@ -57,24 +57,24 @@ assert('Enumerable#include?', '15.3.2.2.10') do
 end
 
 assert('Enumerable#inject', '15.3.2.2.11') do
-  assert_equal [1,2,3,4,5,6].inject() {|s, n| s + n}, 21
-  assert_equal [1,2,3,4,5,6].inject(1) {|s, n| s + n}, 22
+  assert_equal 21, [1,2,3,4,5,6].inject() {|s, n| s + n}
+  assert_equal 22, [1,2,3,4,5,6].inject(1) {|s, n| s + n}
 end
 
 assert('Enumerable#map', '15.3.2.2.12') do
-  assert_equal [1,2,3].map { |i| i + i }, [2,4,6]
+  assert_equal [2,4,6], [1,2,3].map { |i| i + i }
 end
 
 assert('Enumerable#max', '15.3.2.2.13') do
   a = ['aaa', 'bb', 'c']
-  assert_equal a.max, 'c'
-  assert_equal a.max {|i1,i2| i1.length <=> i2.length}, 'aaa'
+  assert_equal 'c', a.max
+  assert_equal 'aaa', a.max {|i1,i2| i1.length <=> i2.length}
 end
 
 assert('Enumerable#min', '15.3.2.2.14') do
   a = ['aaa', 'bb', 'c']
-  assert_equal a.min, 'aaa'
-  assert_equal a.min {|i1,i2| i1.length <=> i2.length}, 'c'
+  assert_equal 'aaa', a.min
+  assert_equal 'c', a.min {|i1,i2| i1.length <=> i2.length}
 end
 
 assert('Enumerable#member?', '15.3.2.2.15') do
@@ -95,14 +95,14 @@ assert('Enumerable#reject', '15.3.2.2.17') do
 end
 
 assert('Enumerable#select', '15.3.2.2.18') do
-  assert_equal [1,2,3,4,5,6,7,8,9].select() {|i| i%2 == 0}, [2,4,6,8]
+  assert_equal [2,4,6,8], [1,2,3,4,5,6,7,8,9].select() {|i| i%2 == 0}
 end
 
 assert('Enumerable#sort', '15.3.2.2.19') do
-  assert_equal [7,3,1,2,6,4].sort, [1,2,3,4,6,7]
-  assert_equal [7,3,1,2,6,4].sort {|e1,e2|e2<=>e1}, [7,6,4,3,2,1]
+  assert_equal [1,2,3,4,6,7], [7,3,1,2,6,4].sort
+  assert_equal [7,6,4,3,2,1], [7,3,1,2,6,4].sort {|e1,e2|e2<=>e1}
 end
 
 assert('Enumerable#to_a', '15.3.2.2.20') do
-  assert_equal [1].to_a, [1]
+  assert_equal [1], [1].to_a
 end
