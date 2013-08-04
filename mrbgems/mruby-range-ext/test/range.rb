d10763190e6c56289ed5dad8cfa9839c1861711a
Daniel Bovensiepen <daniel@bovensiepen.net>
2013-08-04 22:42:06 +0800

    Improve test of mruby-range-ext GEM

--- mrbgems/mruby-range-ext/test/range.rb
+++ mrbgems/mruby-range-ext/test/range.rb
@@ -8,13 +8,13 @@ assert('Range#cover?') do
 end
 
 assert('Range#first') do
-  assert_equal (10..20).first, 10
-  assert_equal (10..20).first(3), [10, 11, 12]
+  assert_equal 10, (10..20).first
+  assert_equal [10, 11, 12], (10..20).first(3)
 end
 
 assert('Range#last') do
-  assert_equal (10..20).last, 20
-  assert_equal (10...20).last, 20
-  assert_equal (10..20).last(3), [18, 19, 20]
-  assert_equal (10...20).last(3), [17, 18, 19]
+  assert_equal 20, (10..20).last
+  assert_equal 20, (10...20).last
+  assert_equal [18, 19, 20], (10..20).last(3)
+  assert_equal [17, 18, 19], (10...20).last(3)
 end
