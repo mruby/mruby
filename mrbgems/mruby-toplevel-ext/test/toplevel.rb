1d58f29e9bf4386c3703e01793f5398a967e9ed3
Daniel Bovensiepen <daniel@bovensiepen.net>
2013-08-04 22:42:39 +0800

    Improve test of mruby-toplevel-ext GEM

--- mrbgems/mruby-toplevel-ext/test/toplevel.rb
+++ mrbgems/mruby-toplevel-ext/test/toplevel.rb
@@ -1,19 +1,19 @@
 ##
 # Toplevel Self(Ext) Test
 
-module ToplevelTestModule1
-  def method_foo
-    :foo
+assert('Toplevel#include') do
+  module ToplevelTestModule1
+    def method_foo
+      :foo
+    end
+
+    CONST_BAR = :bar
   end
 
-  CONST_BAR = :bar
-end
+  module ToplevelTestModule2
+    CONST_BAR = :bar2
+  end
 
-module ToplevelTestModule2
-  CONST_BAR = :bar2
-end
-
-assert('Toplevel#include') do
   self.include ToplevelTestModule2, ToplevelTestModule1
   
   assert_true self.class.included_modules.include?( ToplevelTestModule1 )
