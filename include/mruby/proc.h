e388713532db7fe4817d7e0b55f720eb60bed96f
Jun Hiroe <Jun.Hiroe@gmail.com>
2013-09-01 10:35:15 +0900

    Add super in aliased methods.

--- include/mruby/proc.h
+++ include/mruby/proc.h
@@ -39,6 +39,8 @@ struct RProc {
 #define MRB_ASPEC_KDICT(a)        ((a) & (1<<1))
 #define MRB_ASPEC_BLOCK(a)        ((a) & 1)
 
+#define MRB_PROC_ALIAS_METHOD 64
+#define MRB_PROC_ALIAS_METHOD_P(p) (((p)->flags & MRB_PROC_ALIAS_METHOD) != 0)
 #define MRB_PROC_CFUNC 128
 #define MRB_PROC_CFUNC_P(p) (((p)->flags & MRB_PROC_CFUNC) != 0)
 #define MRB_PROC_STRICT 256
