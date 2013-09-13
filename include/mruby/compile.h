8d34e00102a6ba39b57960e653b0d7b6b3428006
FUKUZAWA-Tadashi <alien@apple.nifty.jp>
2013-09-14 01:00:19 +0900

    fix bugs on Heredocument - heredoc in array literal - heredoc in args - heredoc in expression expand

--- include/mruby/compile.h
+++ include/mruby/compile.h
@@ -128,9 +128,10 @@ struct mrb_parser_state {
   char buf[MRB_PARSER_BUF_SIZE];
   int bidx;
 
-  mrb_ast_node *heredocs;	/* list of mrb_parser_heredoc_info* */
+  mrb_ast_node *all_heredocs;	/* list of mrb_parser_heredoc_info* */
+  mrb_ast_node *heredocs_from_nextline;
   mrb_ast_node *parsing_heredoc;
-  mrb_bool heredoc_starts_nextline:1;
+  mrb_ast_node *lex_strterm_before_heredoc;
   mrb_bool heredoc_end_now:1; /* for mirb */
 
   void *ylval;
