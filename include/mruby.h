febf77594e8a5577ea3373f14c98b098d9b5ec15
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2013-09-23 18:28:47 -0700

    s/uint8_t/enum mrb_fiber_state/

--- include/mruby.h
+++ include/mruby.h
@@ -84,7 +84,7 @@ struct mrb_context {
   struct RProc **ensure;                  /* ensure handler stack */
   int esize;
 
-  uint8_t fib_state;
+  enum mrb_fiber_state fib_state;
   struct RFiber *fib;
 };
 
