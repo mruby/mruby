4ccdaac6e87d68cd8a6e9a9dfe7b0245d762609f
takahashim <maki@rubycolor.org>
2013-11-03 12:12:56 +0900

    parser_dump and codedump_all are not used in mrbc

--- tools/mrbc/mrbc.c
+++ tools/mrbc/mrbc.c
@@ -11,8 +11,6 @@
 
 void mrb_show_version(mrb_state *);
 void mrb_show_copyright(mrb_state *);
-void parser_dump(mrb_state*, struct mrb_ast_node*, int);
-void codedump_all(mrb_state*, int);
 
 struct mrbc_args {
   int argc;
