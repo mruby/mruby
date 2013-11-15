2266d1b5c6dc8d7bd67c9f63352cad61021fc102
fleuria <me.ssword@gmail.com>
2013-11-16 03:43:28 +0800

    add regression test for #1572

--- mrbgems/mruby-bin-mruby/test/mruby.sh
+++ mrbgems/mruby-bin-mruby/test/mruby.sh
@@ -5,3 +5,9 @@ it_should_pass_1564() {
     test "`bin/mruby -e '<<-' 2>&1`" = '-e:1:3: syntax error, unexpected tLSHFT'
 }
 
+it_should_pass_1572() {
+    echo "p 'ok'" > /tmp/mruby1572.rb
+    bin/mrbc -g -o /tmp/mruby1572.mrb /tmp/mruby1572.rb
+    o=`bin/mruby -b /tmp/mruby1572.mrb`
+    test "$o" = '"ok"'
+}
