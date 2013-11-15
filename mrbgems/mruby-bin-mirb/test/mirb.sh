e7425545f160b4f9633736b6f55f355198223703
fleuria <me.ssword@gmail.com>
2013-11-16 04:11:14 +0800

    add regression tests for #1563

---
+++ mrbgems/mruby-bin-mirb/test/mirb.sh
@@ -0,0 +1,11 @@
+describe "regression tests for bin/mirb"
+
+it_should_bahave_good_on_operations() {
+    echo "a=1\nb=2\nc=a+b\n" | bin/mirb | grep '=> 3'
+}
+
+it_should_pass_1563() {
+    o=`echo "a=1;b=2;c=3\nb\nc" | bin/mirb`
+    echo $o | grep '=> 3'
+    echo $o | grep '=> 2'
+}
