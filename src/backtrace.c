5b51b119ca16fe42d63896da8395a5d05bfa9877
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2013-11-18 22:51:51 +0900

    ci->err may be out of boud; close #1583

--- src/backtrace.c
+++ src/backtrace.c
@@ -81,7 +81,7 @@ mrb_output_backtrace(mrb_state *mrb, struct RObject *exc, output_stream_func fun
       mrb_irep *irep = ci->proc->body.irep;
       mrb_code *pc;
 
-      if (mrb->c->cibase[i].err) {
+      if (mrb->c->cibase[i].err && irep && mrb->c->cibase[i].err - irep->iseq < irep->ilen) {
         pc = mrb->c->cibase[i].err;
       }
       else if (i+1 <= ciidx) {
