217595c325a72f250a36a7c5c57b59941aa48118
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2013-11-20 07:38:53 +0900

    need to free pooled string bodies as well

--- src/state.c
+++ src/state.c
@@ -130,8 +130,10 @@ mrb_irep_free(mrb_state *mrb, mrb_irep *irep)
   if (!(irep->flags & MRB_ISEQ_NO_FREE))
     mrb_free(mrb, irep->iseq);
   for (i=0; i<irep->plen; i++) {
-    if (mrb_type(irep->pool[i]) == MRB_TT_STRING)
+    if (mrb_type(irep->pool[i]) == MRB_TT_STRING) {
+      mrb_free(mrb, mrb_str_ptr(irep->pool[i])->ptr);
       mrb_free(mrb, mrb_obj_ptr(irep->pool[i]));
+    }
   }
   mrb_free(mrb, irep->pool);
   mrb_free(mrb, irep->syms);
