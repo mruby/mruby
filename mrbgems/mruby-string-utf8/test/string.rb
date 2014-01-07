7fc9264b0fd124559c3095dbdc515f31a0b35f55
h2so5 <mail@h2so5.net>
2014-01-08 02:21:19 +0900

    add String#reverse, String#reverse! for UTF-8

--- mrbgems/mruby-string-utf8/test/string.rb
+++ mrbgems/mruby-string-utf8/test/string.rb
@@ -9,3 +9,19 @@ assert('String#[]') do
   assert_equal "んに", "こんにちわ世界"[1,2]
   assert_equal "世", "こんにちわ世界"["世"]
 end
+
+assert('String#reverse', '15.2.10.5.29') do
+  a = 'こんにちわ世界!'
+  a.reverse
+    
+  assert_equal 'こんにちわ世界!', a
+  assert_equal '!界世わちにんこ', 'こんにちわ世界!'.reverse
+end
+
+assert('String#reverse!', '15.2.10.5.30') do
+  a = 'こんにちわ世界!'
+  a.reverse!
+    
+  assert_equal '!界世わちにんこ', a
+  assert_equal '!界世わちにんこ', 'こんにちわ世界!'.reverse!
+end
