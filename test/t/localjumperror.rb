565e1f6ef2fb149e7519b49276f5ea80ef72decc
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2014-02-05 12:58:01 +0900

    disable LocalJumpError test since the code causes SyntaxError (not LocalJumpError) on CRuby

--- test/t/localjumperror.rb
+++ test/t/localjumperror.rb
@@ -3,10 +3,10 @@
 
 assert('LocalJumpError', '15.2.25') do
   assert_equal Class, LocalJumpError.class
-  assert_raise LocalJumpError do
-    # this will cause an exception due to the wrong location
-    retry
-  end
+#  assert_raise LocalJumpError do
+#    # this will cause an exception due to the wrong location
+#    retry
+#  end
 end
 
 # TODO 15.2.25.2.1 LocalJumpError#exit_value
