95e2be6508257af3ed4989b39c02d281ae6c5330
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2014-02-07 12:24:24 +0900

    forgot to add new files

---
+++ include/mruby/version.h
@@ -0,0 +1,26 @@
+#define MRUBY_RUBY_VERSION "1.9"
+#define MRUBY_VERSION "v1.0.0"
+#define MRUBY_PATCHLEVEL 0
+#define MRUBY_REVISION 0
+
+#define MRUBY_RELEASE_DATE "2014-01-10"
+#define MRUBY_RELEASE_YEAR 2014
+#define MRUBY_RELEASE_MONTH 1
+#define MRUBY_RELEASE_DAY 10
+
+#define MRUBY_BIRTH_YEAR 2010
+
+#define MRUBY_AUTHOR "mruby developers"
+
+#define STRINGIZE0(expr) #expr
+#define STRINGIZE(expr) STRINGIZE0(expr)
+
+#define MRUBY_DESCRIPTION             \
+    "mruby "MRUBY_VERSION             \
+    " ("MRUBY_RELEASE_DATE") " 
+#define MRUBY_COPYRIGHT               \
+    "mruby - Copyright (c) "          \
+    STRINGIZE(MRUBY_BIRTH_YEAR)"-"    \
+    STRINGIZE(MRUBY_RELEASE_YEAR)" "  \
+    MRUBY_AUTHOR
+
