d8a5f1205338f8ed664745ef74c825b74b9bb081
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2014-02-08 13:56:15 +0900

    forget to add function prototypes

--- include/mruby.h
+++ include/mruby.h
@@ -186,6 +186,8 @@ struct RClass * mrb_module_new(mrb_state *mrb);
 mrb_bool mrb_class_defined(mrb_state *mrb, const char *name);
 struct RClass * mrb_class_get(mrb_state *mrb, const char *name);
 struct RClass * mrb_class_get_under(mrb_state *mrb, struct RClass *outer, const char *name);
+struct RClass * mrb_module_get(mrb_state *mrb, const char *name);
+struct RClass * mrb_module_get_under(mrb_state *mrb, struct RClass *outer, const char *name);
 
 mrb_value mrb_obj_dup(mrb_state *mrb, mrb_value obj);
 mrb_value mrb_check_to_integer(mrb_state *mrb, mrb_value val, const char *method);
