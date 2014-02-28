d60944ef3aaebefa3380cc984d32c127b1ed1782
chasonr <chasonr@gmail.com>
2014-02-27 23:42:07 -0500

    Test for UTF-8 as mattn suggests.
    The string is "Hello world" in Japanese.  (hat tip:  Google Translate.)

--- mrbgems/mruby-numeric-ext/test/numeric.rb
+++ mrbgems/mruby-numeric-ext/test/numeric.rb
@@ -5,7 +5,7 @@ assert('Integer#chr') do
   assert_equal("A", 65.chr)
   assert_equal("B", 0x42.chr)
 
-  if 255.chr.bytes == [195, 191] then
+  if "こんにちわ世界".size == 7 then
       # UTF-8 gem is configured
       assert_raise(RangeError) { 0x110000.chr }
   else
