7074a5591389b6b4f9c88a3334dc49c841f8c22f
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2014-03-01 10:53:38 +0900

    revert changes to mrbconf.h

--- include/mrbconf.h
+++ include/mrbconf.h
@@ -9,61 +9,61 @@
 
 /* configuration options: */
 /* add -DMRB_USE_FLOAT to use float instead of double for floating point numbers */
-/* #define MRB_USE_FLOAT */
+//#define MRB_USE_FLOAT
 
 /* add -DMRB_INT16 to use 16bit integer for mrb_int; conflict with MRB_INT64 */
-/* #define MRB_INT16 */
+//#define MRB_INT16
 
 /* add -DMRB_INT64 to use 64bit integer for mrb_int; conflict with MRB_INT16 */
-/* #define MRB_INT64 */
+//#define MRB_INT64
 
 /* represent mrb_value in boxed double; conflict with MRB_USE_FLOAT */
-/* #define MRB_NAN_BOXING */
+//#define MRB_NAN_BOXING
 
 /* define on big endian machines; used by MRB_NAN_BOXING */
-/* #define MRB_ENDIAN_BIG */
+//#define MRB_ENDIAN_BIG
 
 /* represent mrb_value as a word (natural unit of data for the processor) */
-/* #define MRB_WORD_BOXING */
+// #define MRB_WORD_BOXING
 
 /* argv max size in mrb_funcall */
-/* #define MRB_FUNCALL_ARGC_MAX 16 */
+//#define MRB_FUNCALL_ARGC_MAX 16
 
 /* number of object per heap page */
-/* #define MRB_HEAP_PAGE_SIZE 1024 */
+//#define MRB_HEAP_PAGE_SIZE 1024
 
 /* use segmented list for IV table */
-/* #define MRB_USE_IV_SEGLIST */
+//#define MRB_USE_IV_SEGLIST
 
 /* initial size for IV khash; ignored when MRB_USE_IV_SEGLIST is set */
-/* #define MRB_IVHASH_INIT_SIZE 8 */
+//#define MRB_IVHASH_INIT_SIZE 8
 
 /* turn off generational GC by default */
-/* #define MRB_GC_TURN_OFF_GENERATIONAL */
+//#define MRB_GC_TURN_OFF_GENERATIONAL
 
 /* default size of khash table bucket */
-/* #define KHASH_DEFAULT_SIZE 32 */
+//#define KHASH_DEFAULT_SIZE 32
 
 /* allocated memory address alignment */
-/* #define POOL_ALIGNMENT 4 */
+//#define POOL_ALIGNMENT 4
 
 /* page size of memory pool */
-/* #define POOL_PAGE_SIZE 16000 */
+//#define POOL_PAGE_SIZE 16000
 
 /* initial minimum size for string buffer */
-/* #define MRB_STR_BUF_MIN_SIZE 128 */
+//#define MRB_STR_BUF_MIN_SIZE 128
 
 /* arena size */
-/* #define MRB_GC_ARENA_SIZE 100 */
+//#define MRB_GC_ARENA_SIZE 100
 
 /* fixed size GC arena */
-/* #define MRB_GC_FIXED_ARENA */
+//#define MRB_GC_FIXED_ARENA
 
 /* -DDISABLE_XXXX to drop following features */
-/* #define DISABLE_STDIO */ /* use of stdio */
+//#define DISABLE_STDIO		/* use of stdio */
 
 /* -DENABLE_XXXX to enable following features */
-/* #define ENABLE_DEBUG */ /* hooks for debugger */
+//#define ENABLE_DEBUG		/* hooks for debugger */
 
 /* end of configuration */
 
