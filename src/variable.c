bfc522bcd56b0be97c16a80dc00174eca5dd4fcc
cremno <cremno@mail.ru>
2014-03-08 18:38:31 +0100

    use mrb_str_cat_lit for literals

--- src/variable.c
+++ src/variable.c
@@ -596,7 +596,7 @@ mrb_obj_iv_inspect(mrb_state *mrb, struct RObject *obj)
     const char *cn = mrb_obj_classname(mrb, mrb_obj_value(obj));
     mrb_value str = mrb_str_buf_new(mrb, 30);
 
-    mrb_str_buf_cat(mrb, str, "-<", 2);
+    mrb_str_cat_lit(mrb, str, "-<");
     mrb_str_cat_cstr(mrb, str, cn);
     mrb_str_cat_lit(mrb, str, ":");
     mrb_str_concat(mrb, str, mrb_ptr_to_str(mrb, obj));
