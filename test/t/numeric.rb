be58f1c9ab7db9451f9096f717ce650b9700c076
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2014-03-28 11:05:53 +0900

    add Numeric#/ tests; ref #1965

--- test/t/numeric.rb
+++ test/t/numeric.rb
@@ -22,6 +22,16 @@ assert('Numeric#abs', '15.2.7.4.3') do
   assert_equal(1.0, -1.abs)
 end
 
+assert('Numeric#/', '15.2.8.3.4') do
+  n = Class.new(Numeric){ def /(x); 15.1;end }.new
+
+  assert_equal(2, 10/5)
+  assert_equal(0.0625, 1/16)
+  assert_equal(15.1, n/10)
+  assert_raise(TypeError){ 1/n }
+  assert_raise(TypeError){ 1/nil }
+end
+
 # Not ISO specified
 
 assert('Numeric#**') do
