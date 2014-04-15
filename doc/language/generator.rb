a182a0d38cd4bdc0012c72fa0e1fc841a62b980e
Daniel Bovensiepen <daniel@bovensiepen.net>
2014-04-15 13:17:23 +0800

    Improve generator to output status

--- doc/language/generator.rb
+++ doc/language/generator.rb
@@ -1,7 +1,12 @@
 #!/usr/bin/env ruby
 
+require 'pty'
+
 c_dir = File.dirname(__FILE__)
 MRUBY_ROOT = File.expand_path("#{c_dir}/../..")
 DOC_DIR = File.expand_path(c_dir)
 
-puts `#{DOC_DIR}/mrbdoc/mrbdoc.rb #{MRUBY_ROOT} #{DOC_DIR}`
+cmd = "#{DOC_DIR}/mrbdoc/mrbdoc.rb #{MRUBY_ROOT} #{DOC_DIR}"
+PTY.spawn(cmd) do |i,o,pid|
+  i.each { |l| print l }
+end
