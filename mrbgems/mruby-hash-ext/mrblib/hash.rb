015f60fcad03ec697d8489281b3718929d242b33
Jun Hiroe <Jun.Hiroe@gmail.com>
2014-05-05 11:15:48 +0900

    Add Hash#invert

--- mrbgems/mruby-hash-ext/mrblib/hash.rb
+++ mrbgems/mruby-hash-ext/mrblib/hash.rb
@@ -120,4 +120,21 @@ class Hash
   def flatten(level=1)
     self.to_a.flatten(level)
   end
+
+  ##
+  #  call-seq:
+  #     hsh.invert -> new_hash
+  #
+  #  Returns a new hash created by using <i>hsh</i>'s values as keys, and
+  #  the keys as values.
+  #
+  #     h = { "n" => 100, "m" => 100, "y" => 300, "d" => 200, "a" => 0 }
+  #     h.invert   #=> {0=>"a", 100=>"m", 200=>"d", 300=>"y"}
+  #
+
+  def invert
+    h = Hash.new
+    self.each {|k, v| h[v] = k }
+    h
+  end
 end
