2f1b4eb14469e2db3453c237f00eeccbb7093fc2
Jun Hiroe <Jun.Hiroe@gmail.com>
2014-05-11 03:54:07 +0900

    Refactor mrb_obj_equal_m()

--- src/kernel.c
+++ src/kernel.c
@@ -93,12 +93,9 @@ static mrb_value
 mrb_obj_equal_m(mrb_state *mrb, mrb_value self)
 {
   mrb_value arg;
-  mrb_bool eql_p;
 
   mrb_get_args(mrb, "o", &arg);
-  eql_p = mrb_obj_equal(mrb, self, arg);
-
-  return mrb_bool_value(eql_p);
+  return mrb_bool_value(mrb_obj_equal(mrb, self, arg));
 }
 
 static mrb_value
