066c2772e88dff2fcdf11c18d6396b5055cc7b61
ksss <co000ri@gmail.com>
2014-05-16 22:26:47 +0900

    const_missing error message more detail

--- src/class.c
+++ src/class.c
@@ -1890,8 +1890,16 @@ mrb_mod_const_missing(mrb_state *mrb, mrb_value mod)
   mrb_sym sym;
 
   mrb_get_args(mrb, "n", &sym);
-  mrb_name_error(mrb, sym, "uninitialized constant %S",
-                 mrb_sym2str(mrb, sym));
+
+  if (mrb_class_real(mrb_class_ptr(mod)) != mrb->object_class) {
+    mrb_name_error(mrb, sym, "uninitialized constant %S::%S",
+                   mod,
+                   mrb_sym2str(mrb, sym));
+  }
+  else {
+    mrb_name_error(mrb, sym, "uninitialized constant %S",
+                   mrb_sym2str(mrb, sym));
+  }
   /* not reached */
   return mrb_nil_value();
 }
