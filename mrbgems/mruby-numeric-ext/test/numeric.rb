b6eccd0721c3995cab2a525e73483057846f3ee1
take_cheeze <takechi101010@gmail.com>
2014-05-26 21:45:50 +0900

    Move UTF-8 Integer#chr test to mruby-string-utf8. And skip it in mruby-numeric-ext if mruby-string-utf8 is enabled.

--- mrbgems/mruby-numeric-ext/test/numeric.rb
+++ mrbgems/mruby-numeric-ext/test/numeric.rb
@@ -2,16 +2,14 @@
 # Numeric(Ext) Test
 
 assert('Integer#chr') do
+  # UTF-8 gem is configured
+  skip if "こんにちわ世界".size == 7
+
   assert_equal("A", 65.chr)
   assert_equal("B", 0x42.chr)
 
-  if "こんにちわ世界".size == 7 then
-      # UTF-8 gem is configured
-      assert_raise(RangeError) { 0x110000.chr }
-  else
-      # multibyte encoding (not support yet)
-      assert_raise(RangeError) { 256.chr }
-  end
+  # multibyte encoding (not support yet)
+  assert_raise(RangeError) { 256.chr }
 end
 
 assert('Integer#div') do
