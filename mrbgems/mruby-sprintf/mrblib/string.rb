c3ed6e3d25ff3b6c4980ae8f2c4d0665a1d95d69
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2014-06-04 09:08:22 +0900

    use is_a? Array for String#% argument check as the original PR did in #2349

--- mrbgems/mruby-sprintf/mrblib/string.rb
+++ mrbgems/mruby-sprintf/mrblib/string.rb
@@ -1,9 +1,9 @@
 class String
   def %(args)
-    if args.is_a? Hash
-      sprintf(self, args)
-    else
+    if args.is_a? Array
       sprintf(self, *args)
+    else
+      sprintf(self, args)
     end
   end
 end
