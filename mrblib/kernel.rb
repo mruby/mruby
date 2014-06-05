799c9b30f60a0fd5cc3a04f4121cee94b5d7a192
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2014-06-05 21:41:28 +0900

    remove eval (that only raises NotImplementedError) from the core; ref #2354

--- mrblib/kernel.rb
+++ mrblib/kernel.rb
@@ -4,39 +4,24 @@
 # ISO 15.3.1
 module Kernel
 
-  # 15.3.1.2.1
-  def self.`(s)
-    raise NotImplementedError.new("` not implemented")
-  end
-
+  # 15.3.1.2.1 Kernel.`
+  # provided by Kernel#`
   # 15.3.1.3.5
   def `(s)
-    Kernel.`(s)
+    raise NotImplementedError.new("backquotes not implemented")
   end
+
+  ##
+  # 15.3.1.2.3  Kernel.eval
+  # 15.3.1.3.12 Kernel#eval
+  # NotImplemented by mruby core; use mruby-eval gem
+
+  ##
+  # ISO 15.3.1.2.8 Kernel.loop
+  # provided by Kernel#loop
 
   ##
   # Calls the given block repetitively.
-  #
-  # ISO 15.3.1.2.8
-  # provided by Kernel#loop
-  # def self.loop #(&block)
-  #   while(true)
-  #     yield
-  #   end
-  # end
-
-  # 15.3.1.2.3
-  def self.eval(s)
-    raise NotImplementedError.new("eval not implemented")
-  end
-
-  # 15.3.1.3.12
-  def eval(s)
-    Kernel.eval(s)
-  end
-
-  ##
-  # Alias for +Kernel.loop+.
   #
   # ISO 15.3.1.3.29
   def loop
