03866f25f86a4f0d0f924b664414fedaac31319d
take_cheeze <takechi101010@gmail.com>
2014-07-10 21:43:07 +0900

    Run mrbgem and core tests on minimum dependencies. Solves #2355.
    In test drivers:
    * Uses `mrb_t_pass_result` to check and pass test result to main `mrb_state`.
    * Adds `mrb_init_test_driver` to init test `mrb_state`.

--- mrbgems/mruby-hash-ext/mrbgem.rake
+++ mrbgems/mruby-hash-ext/mrbgem.rake
@@ -2,4 +2,6 @@ MRuby::Gem::Specification.new('mruby-hash-ext') do |spec|
   spec.license = 'MIT'
   spec.author  = 'mruby developers'
   spec.summary = 'extensional Hash class'
+  spec.add_dependency 'mruby-enum-ext', :core => 'mruby-enum-ext'
+  spec.add_dependency 'mruby-array-ext', :core => 'mruby-array-ext'
 end
--- test/init_mrbtest.c
+++ test/init_mrbtest.c
@@ -6,15 +6,26 @@ extern const uint8_t mrbtest_assert_irep[];
 extern const uint8_t mrbtest_irep[];
 
 void mrbgemtest_init(mrb_state* mrb);
+void mrb_init_test_driver(mrb_state* mrb);
+void mrb_t_pass_result(mrb_state *mrb_dst, mrb_state *mrb_src);
 
 void
 mrb_init_mrbtest(mrb_state *mrb)
 {
+  mrb_state *core_test;
+
   mrb_load_irep(mrb, mrbtest_assert_irep);
-  mrb_load_irep(mrb, mrbtest_irep);
+
+  core_test = mrb_open_core(mrb_default_allocf, NULL);
+  mrb_init_test_driver(core_test);
+  mrb_load_irep(core_test, mrbtest_assert_irep);
+  mrb_load_irep(core_test, mrbtest_irep);
+  mrb_t_pass_result(mrb, core_test);
+
 #ifndef DISABLE_GEMS
   mrbgemtest_init(mrb);
 #endif
+
   if (mrb->exc) {
     mrb_print_error(mrb);
     exit(EXIT_FAILURE);
