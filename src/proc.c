6c1dfc95669ca6c39685184c58760acc9f799d64
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2014-08-13 23:58:32 +0900

    need to add space for receiver to proc env; fix #2525

--- src/proc.c
+++ src/proc.c
@@ -41,7 +41,7 @@ closure_setup(mrb_state *mrb, struct RProc *p, int nlocals)
 
   if (!mrb->c->ci->env) {
     e = (struct REnv*)mrb_obj_alloc(mrb, MRB_TT_ENV, (struct RClass*)mrb->c->ci->proc->env);
-    MRB_ENV_STACK_LEN(e)= (unsigned int)nlocals;
+    MRB_ENV_STACK_LEN(e) = (unsigned int)nlocals+1; /* add space for receiver */
     e->mid = mrb->c->ci->mid;
     e->cioff = mrb->c->ci - mrb->c->cibase;
     e->stack = mrb->c->stack;
