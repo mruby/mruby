d94469ac2320d98cfcc4f6b0e64247c4b9df2edd
kkkkkt <go.kikt@gmail.com>
2014-08-21 13:55:31 +0900

    changed to call check_cv_name_str in check_cv_name_sym and adjust indent

--- src/etc.c
+++ src/etc.c
@@ -113,34 +113,34 @@ mrb_obj_id(mrb_value obj)
 #define MakeID(p)    MakeID2(p,tt)
 
   switch (tt) {
-  case  MRB_TT_FREE:
-  case  MRB_TT_UNDEF:
+  case MRB_TT_FREE:
+  case MRB_TT_UNDEF:
     return MakeID(0); /* not define */
-  case  MRB_TT_FALSE:
+  case MRB_TT_FALSE:
     if (mrb_nil_p(obj))
       return MakeID(1);
     return MakeID(0);
-  case  MRB_TT_TRUE:
+  case MRB_TT_TRUE:
     return MakeID(1);
-  case  MRB_TT_SYMBOL:
+  case MRB_TT_SYMBOL:
     return MakeID(mrb_symbol(obj));
-  case  MRB_TT_FIXNUM:
+  case MRB_TT_FIXNUM:
     return MakeID2(mrb_float_id((mrb_float)mrb_fixnum(obj)), MRB_TT_FLOAT);
-  case  MRB_TT_FLOAT:
+  case MRB_TT_FLOAT:
     return MakeID(mrb_float_id(mrb_float(obj)));
-  case  MRB_TT_STRING:
-  case  MRB_TT_OBJECT:
-  case  MRB_TT_CLASS:
-  case  MRB_TT_MODULE:
-  case  MRB_TT_ICLASS:
-  case  MRB_TT_SCLASS:
-  case  MRB_TT_PROC:
-  case  MRB_TT_ARRAY:
-  case  MRB_TT_HASH:
-  case  MRB_TT_RANGE:
-  case  MRB_TT_EXCEPTION:
-  case  MRB_TT_FILE:
-  case  MRB_TT_DATA:
+  case MRB_TT_STRING:
+  case MRB_TT_OBJECT:
+  case MRB_TT_CLASS:
+  case MRB_TT_MODULE:
+  case MRB_TT_ICLASS:
+  case MRB_TT_SCLASS:
+  case MRB_TT_PROC:
+  case MRB_TT_ARRAY:
+  case MRB_TT_HASH:
+  case MRB_TT_RANGE:
+  case MRB_TT_EXCEPTION:
+  case MRB_TT_FILE:
+  case MRB_TT_DATA:
   default:
     return MakeID(mrb_ptr(obj));
   }
