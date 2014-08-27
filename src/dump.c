921798be01294f45dfcdce2133460a2e9a9f40ec
Tatsuhiko Kubo <cubicdaiya@gmail.com>
2014-08-28 05:19:09 +0900

    Fix error handlings for fprintf().

--- src/dump.c
+++ src/dump.c
@@ -987,11 +987,20 @@ mrb_dump_irep_cfunc(mrb_state *mrb, mrb_irep *irep, int debug_info, FILE *fp, co
 
   result = mrb_dump_irep(mrb, irep, debug_info, &bin, &bin_size);
   if (result == MRB_DUMP_OK) {
-    fprintf(fp, "#include <stdint.h>\n"); /* for uint8_t under at least Darwin */
-    fprintf(fp, "const uint8_t %s[] = {", initname);
+    if (fprintf(fp, "#include <stdint.h>\n") < 0) { /* for uint8_t under at least Darwin */
+      mrb_free(mrb, bin);
+      return MRB_DUMP_WRITE_FAULT;
+    }
+    if (fprintf(fp, "const uint8_t %s[] = {", initname) < 0) {
+      mrb_free(mrb, bin);
+      return MRB_DUMP_WRITE_FAULT;
+    }
     while (bin_idx < bin_size) {
       if (bin_idx % 16 == 0) fputs("\n", fp);
-      fprintf(fp, "0x%02x,", bin[bin_idx++]);
+      if (fprintf(fp, "0x%02x,", bin[bin_idx++]) < 0) {
+        mrb_free(mrb, bin);
+        return MRB_DUMP_WRITE_FAULT;
+      }
     }
     fputs("\n};\n", fp);
   }
