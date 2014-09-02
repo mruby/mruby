f7a7426d7e4f6d3372c2e16296e93c2e31f6598c
Jun Hiroe <Jun.Hiroe@gmail.com>
2014-09-02 19:39:39 +0900

    Refactor mrb_obj_is_kind_of_m() in kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -597,12 +597,10 @@ static mrb_value
 mrb_obj_is_kind_of_m(mrb_state *mrb, mrb_value self)
 {
   mrb_value arg;
-  mrb_bool kind_of_p;
 
   mrb_get_args(mrb, "C", &arg);
-  kind_of_p = mrb_obj_is_kind_of(mrb, self, mrb_class_ptr(arg));
 
-  return mrb_bool_value(kind_of_p);
+  return mrb_bool_value(mrb_obj_is_kind_of(mrb, self, mrb_class_ptr(arg)));
 }
 
 KHASH_DECLARE(st, mrb_sym, char, FALSE)
