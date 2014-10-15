c5a23e6a0dd92edc239401b18f43d2268e290784
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2014-10-15 21:21:58 +0900

    remove header prototype of mrb_exc_print; close #2607

--- include/mruby/error.h
+++ include/mruby/error.h
@@ -22,7 +22,6 @@ MRB_API void mrb_sys_fail(mrb_state *mrb, const char *mesg);
 MRB_API mrb_value mrb_exc_new_str(mrb_state *mrb, struct RClass* c, mrb_value str);
 #define mrb_exc_new_str_lit(mrb, c, lit) mrb_exc_new_str(mrb, c, mrb_str_new_lit(mrb, lit))
 MRB_API mrb_value mrb_make_exception(mrb_state *mrb, int argc, const mrb_value *argv);
-MRB_API void mrb_exc_print(mrb_state *mrb, struct RObject *exc);
 MRB_API mrb_value mrb_exc_backtrace(mrb_state *mrb, mrb_value exc);
 MRB_API mrb_value mrb_get_backtrace(mrb_state *mrb);
 MRB_API mrb_noreturn void mrb_no_method_error(mrb_state *mrb, mrb_sym id, mrb_int argc, const mrb_value *argv, const char *fmt, ...);
