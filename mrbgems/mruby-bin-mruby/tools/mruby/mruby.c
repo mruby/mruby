fcbb7ec49dee53eb9dce42d2feead0aeb065f544
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2014-12-16 19:26:40 +0900

    Fix crash if #inspect does not return a string value

--- mrbgems/mruby-bin-mruby/tools/mruby/mruby.c
+++ mrbgems/mruby-bin-mruby/tools/mruby/mruby.c
@@ -11,8 +11,13 @@
 static void
 p(mrb_state *mrb, mrb_value obj)
 {
-  obj = mrb_funcall(mrb, obj, "inspect", 0);
-  fwrite(RSTRING_PTR(obj), RSTRING_LEN(obj), 1, stdout);
+  mrb_value val;
+
+  val = mrb_funcall(mrb, obj, "inspect", 0);
+  if (!mrb_string_p(val)) {
+    val = mrb_obj_as_string(mrb, obj);
+  }
+  fwrite(RSTRING_PTR(val), RSTRING_LEN(val), 1, stdout);
   putc('\n', stdout);
 }
 #else
