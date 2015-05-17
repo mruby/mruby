4c868a9562af2abfed149e3dd7a4404af534ca1b
cremno <cremno@mail.ru>
2015-05-17 22:25:55 +0200

    remove SET_PROC_VALUE() macro
    It isn't used anymore since the #2791 merge. It's also unlikely to be
    needed again as it wasn't before (its usage was unnecessary).

--- include/mruby/boxing_no.h
+++ include/mruby/boxing_no.h
@@ -42,7 +42,6 @@ typedef struct mrb_value {
 #define SET_FLOAT_VALUE(mrb,r,v) BOXNIX_SET_VALUE(r, MRB_TT_FLOAT, value.f, (v))
 #define SET_SYM_VALUE(r,v) BOXNIX_SET_VALUE(r, MRB_TT_SYMBOL, value.sym, (v))
 #define SET_OBJ_VALUE(r,v) BOXNIX_SET_VALUE(r, (((struct RObject*)(v))->tt), value.p, (v))
-#define SET_PROC_VALUE(r,v) BOXNIX_SET_VALUE(r, MRB_TT_PROC, value.p, v)
 #define SET_CPTR_VALUE(mrb,r,v) BOXNIX_SET_VALUE(r, MRB_TT_CPTR, value.p, v)
 #define SET_UNDEF_VALUE(r) BOXNIX_SET_VALUE(r, MRB_TT_UNDEF, value.i, 0)
 
