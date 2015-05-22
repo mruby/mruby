ffc168f3f21d70fc0d53829b037e07c0b05e94bb
furunkel <julian@linux4you.it>
2015-05-22 18:50:48 +0200

    Update mcache benchmark build config

--- benchmark/build_config_mcache.rb
+++ benchmark/build_config_mcache.rb
@@ -3,67 +3,47 @@ MRuby::Build.new do |conf|
 end
 
 MRuby::Build.new('no_mcache') do |conf|
-  toolchain :gcc
+  toolchain :clang
 
   conf.gembox 'default'
 end
 
-MRuby::Build.new('mcache1') do |conf|
-  toolchain :gcc
+MRuby::Build.new('mcache_monomorphic') do |conf|
+  toolchain :clang
 
   conf.gembox 'default'
   conf.compilers.each do |c|
     c.defines += %w(MRB_ENABLE_METHOD_CACHE)
-    c.defines += %w(MRB_METHOD_CACHE_SIZE=1)
+    c.defines += %w(MRB_METHOD_CACHE_MORPHICITY=1)
   end
 end
 
-MRuby::Build.new('mcache2') do |conf|
-  toolchain :gcc
+MRuby::Build.new('mcache_dimorphic') do |conf|
+  toolchain :clang
 
   conf.gembox 'default'
   conf.compilers.each do |c|
     c.defines += %w(MRB_ENABLE_METHOD_CACHE)
-    c.defines += %w(MRB_METHOD_CACHE_SIZE=2)
+    c.defines += %w(MRB_METHOD_CACHE_MORPHICITY=2)
   end
 end
 
-MRuby::Build.new('mcache3') do |conf|
-  toolchain :gcc
+MRuby::Build.new('mcache_trimorphic') do |conf|
+  toolchain :clang
 
   conf.gembox 'default'
   conf.compilers.each do |c|
     c.defines += %w(MRB_ENABLE_METHOD_CACHE)
-    c.defines += %w(MRB_METHOD_CACHE_SIZE=3)
+    c.defines += %w(MRB_METHOD_CACHE_MORPHICITY=3)
   end
 end
 
-MRuby::Build.new('mcache4') do |conf|
-  toolchain :gcc
+MRuby::Build.new('mcache_tetramorphic') do |conf|
+  toolchain :clang
 
   conf.gembox 'default'
   conf.compilers.each do |c|
     c.defines += %w(MRB_ENABLE_METHOD_CACHE)
-    c.defines += %w(MRB_METHOD_CACHE_SIZE=4)
-  end
-end
-
-MRuby::Build.new('mcache5') do |conf|
-  toolchain :gcc
-
-  conf.gembox 'default'
-  conf.compilers.each do |c|
-    c.defines += %w(MRB_ENABLE_METHOD_CACHE)
-    c.defines += %w(MRB_METHOD_CACHE_SIZE=5)
-  end
-end
-
-MRuby::Build.new('mcache6') do |conf|
-  toolchain :gcc
-
-  conf.gembox 'default'
-  conf.compilers.each do |c|
-    c.defines += %w(MRB_ENABLE_METHOD_CACHE)
-    c.defines += %w(MRB_METHOD_CACHE_SIZE=6)
+    c.defines += %w(MRB_METHOD_CACHE_MORPHICITY=4)
   end
 end
