fe59d1d1735a374b6a616a07adeba10f2ebe598c
furunkel <julian@linux4you.it>
2015-05-22 23:12:44 +0200

    Add different boxings to jit benchmark build config

--- benchmark/build_config_jit.rb
+++ benchmark/build_config_jit.rb
@@ -2,13 +2,31 @@ MRuby::Build.new do |conf|
   toolchain :gcc
 end
 
-MRuby::Build.new('interp') do |conf|
+MRuby::Build.new('no_boxing') do |conf|
   toolchain :clang
 
   conf.gembox 'default'
 end
 
-MRuby::Build.new('jit') do |conf|
+MRuby::Build.new('word_boxing') do |conf|
+  toolchain :clang
+
+  conf.gembox 'default'
+  conf.compilers.each do |c|
+    c.defines += %w(MRB_WORD_BOXING)
+  end
+end
+
+MRuby::Build.new('nan_boxing') do |conf|
+  toolchain :clang
+
+  conf.gembox 'default'
+  conf.compilers.each do |c|
+    c.defines += %w(MRB_NAN_BOXING)
+  end
+end
+
+MRuby::Build.new('no_boxing_jit') do |conf|
   toolchain :clang
 
   conf.gembox 'default'
@@ -17,4 +35,22 @@ MRuby::Build.new('jit') do |conf|
   end
 end
 
+MRuby::Build.new('word_boxing_jit') do |conf|
+  toolchain :clang
 
+  conf.gembox 'default'
+  conf.compilers.each do |c|
+    c.defines += %w(MRB_WORD_BOXING)
+    c.defines += %w(MRB_ENABLE_JIT)
+  end
+end
+
+MRuby::Build.new('nan_boxing_jit') do |conf|
+  toolchain :clang
+
+  conf.gembox 'default'
+  conf.compilers.each do |c|
+    c.defines += %w(MRB_NAN_BOXING)
+    c.defines += %w(MRB_ENABLE_JIT)
+  end
+end
