bd2686d82d233774003fe683e9396f366b152b6d
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2015-06-01 00:13:12 +0900

    singleton_class should not be cloned; close #2815

--- src/kernel.c
+++ src/kernel.c
@@ -330,6 +330,9 @@ mrb_obj_clone(mrb_state *mrb, mrb_value self)
   if (mrb_immediate_p(self)) {
     mrb_raisef(mrb, E_TYPE_ERROR, "can't clone %S", self);
   }
+  if (mrb_type(self) == MRB_TT_SCLASS) {
+    mrb_raise(mrb, E_TYPE_ERROR, "can't clone singleton class");
+  }
   p = (struct RObject*)mrb_obj_alloc(mrb, mrb_type(self), mrb_obj_class(mrb, self));
   p->c = mrb_singleton_class_clone(mrb, self);
   clone = mrb_obj_value(p);
