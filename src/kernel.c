b6121adc7dbedcf0e6fd262de536c2f8c9217120
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2015-06-01 21:30:07 +0900

    singleton_class should not be duped; fix #2815

--- src/kernel.c
+++ src/kernel.c
@@ -369,6 +369,9 @@ mrb_obj_dup(mrb_state *mrb, mrb_value obj)
   if (mrb_immediate_p(obj)) {
     mrb_raisef(mrb, E_TYPE_ERROR, "can't dup %S", obj);
   }
+  if (mrb_type(obj) == MRB_TT_SCLASS) {
+    mrb_raise(mrb, E_TYPE_ERROR, "can't dup singleton class");
+  }
   p = mrb_obj_alloc(mrb, mrb_type(obj), mrb_obj_class(mrb, obj));
   dup = mrb_obj_value(p);
   init_copy(mrb, dup, obj);
