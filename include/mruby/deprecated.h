e90f0d51a3b2ac52aa76fa3c8b803bcc75433468
furunkel <julian@linux4you.it>
2015-06-23 10:35:02 +0200

    Move deprecated macros and functions to dedicated header file

---
+++ include/mruby/deprecated.h
@@ -0,0 +1,10 @@
+/* Deprecated macros and functions */
+
+#define ARGS_REQ(n)         MRB_ARGS_REQ(n)
+#define ARGS_OPT(n)         MRB_ARGS_OPT(n)
+#define ARGS_REST()         MRB_ARGS_REST()
+#define ARGS_POST(n)        MRB_ARGS_POST()
+#define ARGS_KEY(n1,n2)     MRB_ARGS_KEY(n1,n2)
+#define ARGS_BLOCK()        MRB_ARGS_BLOCK()
+#define ARGS_ANY()          MRB_ARGS_ANY()
+#define ARGS_NONE()         MRB_ARGS_NONE()
