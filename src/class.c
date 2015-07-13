81a2b3431c63c969f73e6c0eeaa968acbace0e44
Corey Powell <mistdragon100@gmail.com>
2015-07-13 09:49:51 -0500

    included_modules, origin fix
    Prepended modules would include their origin ICLASS

--- src/class.c
+++ src/class.c
@@ -1000,10 +1000,11 @@ mrb_mod_included_modules(mrb_state *mrb, mrb_value self)
 {
   mrb_value result;
   struct RClass *c = mrb_class_ptr(self);
+  struct RClass *origin = c->origin;
 
   result = mrb_ary_new(mrb);
   while (c) {
-    if (c->tt == MRB_TT_ICLASS) {
+    if (c != origin && c->tt == MRB_TT_ICLASS) {
       if (c->c->tt == MRB_TT_MODULE) {
         mrb_ary_push(mrb, result, mrb_obj_value(c->c));
       }
