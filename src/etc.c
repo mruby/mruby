96c948d812f06c1ad1024f6db5089e47dbd58d23
go.kikuta <go.kikuta@aktsk.jp>
2015-08-11 18:02:12 +0900

    Remove non-need tmp value

--- src/etc.c
+++ src/etc.c
@@ -67,17 +67,15 @@ mrb_data_get_ptr(mrb_state *mrb, mrb_value obj, const mrb_data_type *type)
 MRB_API mrb_sym
 mrb_obj_to_sym(mrb_state *mrb, mrb_value name)
 {
-  mrb_value tmp;
   mrb_sym id;
 
   switch (mrb_type(name)) {
     default:
-      tmp = mrb_check_string_type(mrb, name);
-      if (mrb_nil_p(tmp)) {
-        tmp = mrb_inspect(mrb, name);
-        mrb_raisef(mrb, E_TYPE_ERROR, "%S is not a symbol", tmp);
+      name = mrb_check_string_type(mrb, name);
+      if (mrb_nil_p(name)) {
+        name = mrb_inspect(mrb, name);
+        mrb_raisef(mrb, E_TYPE_ERROR, "%S is not a symbol", name);
       }
-      name = tmp;
       /* fall through */
     case MRB_TT_STRING:
       name = mrb_str_intern(mrb, name);
