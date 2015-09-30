545d649eff73021d515f7a96841b363b0001e9d9
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2015-09-30 15:42:27 +0900

    fix tests on windows.
    'bin/mruby' not work on windows. so correct command name and quoted arguments.

--- test/bintest.rb
+++ test/bintest.rb
@@ -1,6 +1,14 @@
 $:.unshift File.dirname(File.dirname(File.expand_path(__FILE__)))
 require 'test/assert.rb'
 
+def cmd(s)
+  ENV['SHELL'] ? "bin/#{s}" : "bin\\#{s}.exe"
+end
+
+def shellquote(s)
+  ENV['SHELL'] ? "'#{s}'" : "\"#{s}\""
+end
+
 ARGV.each do |gem|
   Dir["#{gem}/bintest/**/*.rb"].each do |file|
     load file
