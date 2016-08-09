024b09de0c3bf9019bf31ccaa4cd4e150b0113da
Carlo - PERI <carlo.prelz@ispw.unibe.ch>
2016-08-09 11:23:49 +0200

    Apparently, in mruby-process gem, Process is now a Module, not a Class

--- src/io.c
+++ src/io.c
@@ -78,7 +78,7 @@ io_set_process_status(mrb_state *mrb, pid_t pid, int status)
 
   c_status = NULL;
   if (mrb_class_defined(mrb, "Process")) {
-    c_process = mrb_class_get(mrb, "Process");
+    c_process = mrb_module_get(mrb, "Process");
     if (mrb_const_defined(mrb, mrb_obj_value(c_process), mrb_intern_cstr(mrb, "Status"))) {
       c_status = mrb_class_get_under(mrb, c_process, "Status");
     }
