176d93d72a9322f7f8e41aea0b06a857b7ba23aa
ksss <co000ri@gmail.com>
2016-11-17 14:57:30 +0900

    Ranges should not rewrite

--- test/t/range.rb
+++ test/t/range.rb
@@ -57,6 +57,8 @@ assert('Range#initialize', '15.2.14.4.9') do
   assert_true a.exclude_end?
   assert_equal (1..10), b
   assert_false b.exclude_end?
+
+  assert_raise(NameError) { (0..1).send(:initialize, 1, 3) }
 end
 
 assert('Range#last', '15.2.14.4.10') do
