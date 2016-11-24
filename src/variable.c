f5a86ca5305d70c16297b67df30ce37c102af183
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2016-11-24 09:24:45 +0900

    Add pointer casting from mrb_malloc(); ref #3267

--- src/variable.c
+++ src/variable.c
@@ -44,7 +44,7 @@ iv_new(mrb_state *mrb)
 {
   iv_tbl *t;
 
-  t = mrb_malloc(mrb, sizeof(iv_tbl));
+  t = (iv_tbl*)mrb_malloc(mrb, sizeof(iv_tbl));
   t->size = 0;
   t->rootseg =  NULL;
   t->last_len = 0;
@@ -102,7 +102,7 @@ iv_put(mrb_state *mrb, iv_tbl *t, mrb_sym sym, mrb_value val)
     return;
   }
 
-  seg = mrb_malloc(mrb, sizeof(segment));
+  seg = (segment*)mrb_malloc(mrb, sizeof(segment));
   if (!seg) return;
   seg->next = NULL;
   seg->key[0] = sym;
