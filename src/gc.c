mruby/mruby

commit 09b11850a5df44d8e0ba5d031c57146e0c41547b

mrb_gc_unregister() to remove one registration; close #3160
when multiple mrb_gc_register() were called for the same object

src/gc.c
@@ -453,7 +453,7 @@ mrb_gc_unregister(mrb_state *mrb, mrb_value obj)
   mrb_sym root = mrb_intern_lit(mrb, GC_ROOT_NAME);
   mrb_value table = mrb_gv_get(mrb, root);
   struct RArray *a;
-  mrb_int i, j;
+  mrb_int i, len;
 
   if (mrb_nil_p(table)) return;
   if (mrb_type(table) != MRB_TT_ARRAY) {
@@ -462,12 +462,14 @@ mrb_gc_unregister(mrb_state *mrb, mrb_value obj)
   }
   a = mrb_ary_ptr(table);
   mrb_ary_modify(mrb, a);
-  for (i=j=0; i<a->len; i++) {
-    if (!mrb_obj_eq(mrb, a->ptr[i], obj)) {
-      a->ptr[j++] = a->ptr[i];
+  len = a->len-1;
+  for (i=0; i<len; i++) {
+    if (mrb_obj_eq(mrb, a->ptr[i], obj)) {
+      memmove(&a->ptr[i], &a->ptr[i+1], len-i);
+      break;
     }
   }
-  a->len = j;
+  a->len--;
 }
 
 MRB_API struct RBasic*

commit c2e749f878049204239ad63ac9546253aeb6ceeb

fix issues of mrb_gc_unregister introduced in 09b1185
* fixes partial copy of objects in GC root array (due to missing `* sizeof(mrb_value)`)
* restores the behavior that permitted an unregistered object to be used as an argument

src/gc.c
@@ -453,7 +453,7 @@ mrb_gc_unregister(mrb_state *mrb, mrb_value obj)
   mrb_sym root = mrb_intern_lit(mrb, GC_ROOT_NAME);
   mrb_value table = mrb_gv_get(mrb, root);
   struct RArray *a;
-  mrb_int i, len;
+  mrb_int i;
 
   if (mrb_nil_p(table)) return;
   if (mrb_type(table) != MRB_TT_ARRAY) {
@@ -462,14 +462,13 @@ mrb_gc_unregister(mrb_state *mrb, mrb_value obj)
   }
   a = mrb_ary_ptr(table);
   mrb_ary_modify(mrb, a);
-  len = a->len-1;
-  for (i=0; i<len; i++) {
+  for (i = 0; i < a->len; i++) {
     if (mrb_obj_eq(mrb, a->ptr[i], obj)) {
-      memmove(&a->ptr[i], &a->ptr[i+1], len-i);
+      a->len--;
+      memmove(&a->ptr[i], &a->ptr[i + 1], (a->len - i) * sizeof(a->ptr[i]));
       break;
     }
   }
-  a->len--;
 }
 
 MRB_API struct RBasic*