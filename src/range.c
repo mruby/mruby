4d38cad31b2107d23fa393561eb5dae5da16c82c
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2017-01-23 15:05:12 +0900

    Add MRB_API to mrb_range_beg_len(); ref #3411

--- src/range.c
+++ src/range.c
@@ -248,7 +248,7 @@ mrb_range_include(mrb_state *mrb, mrb_value range)
   return mrb_bool_value(include_p);
 }
 
-int
+MRB_API int
 mrb_range_beg_len(mrb_state *mrb, mrb_value range, mrb_int *begp, mrb_int *lenp, mrb_int len, mrb_bool trunc)
 {
   mrb_int beg, end;
