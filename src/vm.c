9e93d5dbf0c96cbf5cc0bc5a9c5f734248d7c37b
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2017-02-27 16:17:57 +0900

    Stack may be reallocated in mrb_run(); fix #3465

--- src/vm.c
+++ src/vm.c
@@ -289,7 +289,7 @@ ecall(mrb_state *mrb, int i)
   mrb_value *self = mrb->c->stack;
   struct RObject *exc;
   int cioff;
-  mrb_value *nstk;
+  ptrdiff_t nstk;
 
   if (i<0) return;
   p = mrb->c->ensure[i];
@@ -298,7 +298,7 @@ ecall(mrb_state *mrb, int i)
     mrb->c->ci->eidx = i;
   cioff = mrb->c->ci - mrb->c->cibase;
   ci = cipush(mrb);
-  nstk = ci->stackent;
+  nstk = ci->stackent - mrb->c->stbase;
   ci->stackent = mrb->c->stack;
   ci->mid = ci[-1].mid;
   ci->acc = CI_ACC_SKIP;
@@ -310,7 +310,7 @@ ecall(mrb_state *mrb, int i)
   exc = mrb->exc; mrb->exc = 0;
   mrb_run(mrb, p, *self);
   mrb->c->ensure[i] = NULL;
-  ci->stackent = nstk;
+  ci->stackent = mrb->c->stbase + nstk;
   mrb->c->ci = mrb->c->cibase + cioff;
   if (!mrb->exc) mrb->exc = exc;
 }
