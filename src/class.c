fa502b498ed421a7bc95fb675ae0f4c6e2f1c16f
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2017-03-18 18:06:37 +0900

    Need to setup singleton_class chain; fix #3509

--- src/class.c
+++ src/class.c
@@ -97,6 +97,7 @@ prepare_singleton_class(mrb_state *mrb, struct RBasic *o)
   }
   else {
     sc->super = o->c;
+    prepare_singleton_class(mrb, (struct RBasic*)sc);
   }
   o->c = sc;
   mrb_field_write_barrier(mrb, (struct RBasic*)o, (struct RBasic*)sc);
