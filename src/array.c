6db38c376880651b76263b16788c36c6c9ea3aef
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2017-03-18 22:38:45 +0900

    Need not to call mrb_obj_as_string() is sep is nil.

--- src/array.c
+++ src/array.c
@@ -1071,7 +1071,9 @@ join_ary(mrb_state *mrb, mrb_value ary, mrb_value sep, mrb_value list)
 MRB_API mrb_value
 mrb_ary_join(mrb_state *mrb, mrb_value ary, mrb_value sep)
 {
-  sep = mrb_obj_as_string(mrb, sep);
+  if (!mrb_nil_p(sep)) {
+    sep = mrb_obj_as_string(mrb, sep);
+  }
   return join_ary(mrb, ary, sep, mrb_ary_new(mrb));
 }
 
