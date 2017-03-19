527dcd52478567c30547d10c87c867cc01865a82
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2017-03-19 20:58:34 +0900

    `super class error` formats the superclass by `inspect`; rerf #3515

--- src/class.c
+++ src/class.c
@@ -267,7 +267,8 @@ mrb_vm_define_class(mrb_state *mrb, mrb_value outer, mrb_value super, mrb_sym id
 
   if (!mrb_nil_p(super)) {
     if (mrb_type(super) != MRB_TT_CLASS) {
-      mrb_raisef(mrb, E_TYPE_ERROR, "superclass must be a Class (%S given)", super);
+      mrb_raisef(mrb, E_TYPE_ERROR, "superclass must be a Class (%S given)",
+                 mrb_inspect(mrb, super));
     }
     s = mrb_class_ptr(super);
   }
