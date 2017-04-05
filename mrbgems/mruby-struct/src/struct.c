bd7bf260e8946f721e3fb81b84c24bb31e92ab90
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2017-04-05 17:52:26 +0900

    Should specify instance type of Structs; fix #3591

--- mrbgems/mruby-struct/src/struct.c
+++ mrbgems/mruby-struct/src/struct.c
@@ -679,6 +679,7 @@ mrb_mruby_struct_gem_init(mrb_state* mrb)
 {
   struct RClass *st;
   st = mrb_define_class(mrb, "Struct",  mrb->object_class);
+  MRB_SET_INSTANCE_TT(st, MRB_TT_ARRAY);
 
   mrb_define_class_method(mrb, st, "new",             mrb_struct_s_def,       MRB_ARGS_ANY());  /* 15.2.18.3.1  */
 
