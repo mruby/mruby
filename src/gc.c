761493934e19d1a6edea53e9fbdb39eb78ef898e
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2017-04-17 16:06:18 +0900

    Mark whole root objects in final_marking_phase(); fix #3603
    ref #1359 #1362

--- src/gc.c
+++ src/gc.c
@@ -996,7 +996,7 @@ incremental_marking_phase(mrb_state *mrb, mrb_gc *gc, size_t limit)
 static void
 final_marking_phase(mrb_state *mrb, mrb_gc *gc)
 {
-  mark_context_stack(mrb, mrb->root_c);
+  mark_context(mrb, mrb->root_c);
   gc_mark_gray_list(mrb, gc);
   mrb_assert(gc->gray_list == NULL);
   gc->gray_list = gc->atomic_gray_list;
