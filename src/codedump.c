f269d1eb4009877765298242754b428a241f642b
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2017-05-26 11:23:27 +0900

    Distinguish OP_R_NORMAL and OP_R_RETURN.

--- src/codedump.c
+++ src/codedump.c
@@ -239,6 +239,7 @@ codedump(mrb_state *mrb, mrb_irep *irep)
       printf("OP_RETURN\tR%d", GETARG_A(c));
       switch (GETARG_B(c)) {
       case OP_R_NORMAL:
+        printf("\tnormal\t"); break;
       case OP_R_RETURN:
         printf("\treturn\t"); break;
       case OP_R_BREAK:
