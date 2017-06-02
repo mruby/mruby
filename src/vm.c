079f310fbc9c23b97f062230c32bd91ac65e4835
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2017-06-02 11:26:32 +0900

    Adjust ridx if `mrb_yield` terminated by `break`; fix #3686
    This issue is related to #3640.

--- src/vm.c
+++ src/vm.c
@@ -283,8 +283,8 @@ cipop(mrb_state *mrb)
   struct mrb_context *c = mrb->c;
   struct REnv *env = c->ci->env;
 
+  c->ridx = c->ci->rpos;
   c->ci--;
-
   if (env) {
     mrb_env_unshare(mrb, env);
   }
