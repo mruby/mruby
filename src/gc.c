819ccd3b859427e38d0b4e5a22ec0005b68c3b31
Yukihiro "Matz" Matsumoto <matz@ruby-lang.org>
2017-06-17 08:30:33 +0900

    Mark the root context in `final_marking_phase`.
    Because the root context cannot be marked from fibers.

--- src/gc.c
+++ src/gc.c
@@ -997,6 +997,7 @@ final_marking_phase(mrb_state *mrb, mrb_gc *gc)
 {
   mrb_gc_mark_gv(mrb);
   mark_context(mrb, mrb->c);
+  mark_context(mrb, mrb->root_c);
   mrb_gc_mark(mrb, (struct RBasic*)mrb->exc);
   gc_mark_gray_list(mrb, gc);
   mrb_assert(gc->gray_list == NULL);
