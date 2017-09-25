b406e9f7954d9ce9cec2d07e7d3d8627036a821a
Tomasz Dąbrowski <t.dabrowski@rock-hard.eu>
2017-09-25 16:07:16 +0200

    fix: src\gc.c(1392): warning C4244: '=': conversion from 'mrb_int' to 'int', possible loss of data

--- src/gc.c
+++ src/gc.c
@@ -1389,7 +1389,7 @@ gc_interval_ratio_set(mrb_state *mrb, mrb_value obj)
   mrb_int ratio;
 
   mrb_get_args(mrb, "i", &ratio);
-  mrb->gc.interval_ratio = ratio;
+  mrb->gc.interval_ratio = (int)ratio;
   return mrb_nil_value();
 }
 
