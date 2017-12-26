6b09692684583fd4d551bd53b00e0e90b34ebab5
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2017-12-26 09:44:39 +0900

    Add `Integer#{allbits?,anybits?,nobits?}. [Ruby2.5]
    In mruby, those methods are defined in `Integral` module.

--- mrbgems/mruby-numeric-ext/src/numeric_ext.c
+++ mrbgems/mruby-numeric-ext/src/numeric_ext.c
@@ -37,12 +37,63 @@ mrb_int_chr(mrb_state *mrb, mrb_value x)
   return mrb_str_new(mrb, &c, 1);
 }
 
+/*
+ *  call-seq:
+ *     int.allbits?(mask)  ->  true or false
+ *
+ *  Returns +true+ if all bits of <code>+int+ & +mask+</code> are 1.
+ */
+static mrb_value
+mrb_int_allbits(mrb_state *mrb, mrb_value self)
+{
+  mrb_int n, m;
+
+  n = to_int(self);
+  mrb_get_args(mrb, "i", &m);
+  return mrb_bool_value((n & m) == m);
+}
+
+/*
+ *  call-seq:
+ *     int.anybits?(mask)  ->  true or false
+ *
+ *  Returns +true+ if any bits of <code>+int+ & +mask+</code> are 1.
+ */
+static mrb_value
+mrb_int_anybits(mrb_state *mrb, mrb_value self)
+{
+  mrb_int n, m;
+
+  n = to_int(self);
+  mrb_get_args(mrb, "i", &m);
+  return mrb_bool_value((n & m) != 0);
+}
+
+/*
+ *  call-seq:
+ *     int.nobits?(mask)  ->  true or false
+ *
+ *  Returns +true+ if no bits of <code>+int+ & +mask+</code> are 1.
+ */
+static mrb_value
+mrb_int_nobits(mrb_state *mrb, mrb_value self)
+{
+  mrb_int n, m;
+
+  n = to_int(self);
+  mrb_get_args(mrb, "i", &m);
+  return mrb_bool_value((n & m) == 0);
+}
+
 void
 mrb_mruby_numeric_ext_gem_init(mrb_state* mrb)
 {
   struct RClass *i = mrb_module_get(mrb, "Integral");
 
   mrb_define_method(mrb, i, "chr", mrb_int_chr, MRB_ARGS_NONE());
+  mrb_define_method(mrb, i, "allbits?", mrb_int_allbits, MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, i, "anybits?", mrb_int_anybits, MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, i, "nobits?", mrb_int_nobits, MRB_ARGS_REQ(1));
 }
 
 void
