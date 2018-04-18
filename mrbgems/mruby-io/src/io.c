05db341ef5454ba5bbf613f7f84890fd16c9d532
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2018-04-18 17:25:52 +0900

    `mrb_dup()` should fail if `fd` is a negative integer; ref #4005

--- mrbgems/mruby-io/src/io.c
+++ mrbgems/mruby-io/src/io.c
@@ -542,12 +542,12 @@ mrb_dup(mrb_state *mrb, int fd, mrb_bool *failed)
 {
   int new_fd;
 
-  *failed = FALSE;
+  *failed = TRUE;
   if (fd < 0)
     return fd;
 
   new_fd = dup(fd);
-  if (new_fd == -1) *failed = TRUE;
+  if (new_fd > 0) *failed = FALSE;
   return new_fd;
 }
 
