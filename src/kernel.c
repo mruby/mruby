1dddc2f712f774b22a8f65432299cebb3b26361b
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2018-05-30 17:05:05 +0900

    Clear `__classname__` of duped class/module; ref #4027

--- src/kernel.c
+++ src/kernel.c
@@ -282,7 +282,9 @@ init_copy(mrb_state *mrb, mrb_value dest, mrb_value obj)
     case MRB_TT_CLASS:
     case MRB_TT_MODULE:
       copy_class(mrb, dest, obj);
-      /* fall through */
+      mrb_iv_copy(mrb, dest, obj);
+      mrb_iv_remove(mrb, dest, mrb_intern_lit(mrb, "__classname__"));
+      break;
     case MRB_TT_OBJECT:
     case MRB_TT_SCLASS:
     case MRB_TT_HASH:
