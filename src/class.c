faa4eaf6803bd11669bc324b4c34e7162286bfa3
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2018-06-08 14:13:06 +0900

    `mrb_class_real()` did not work for `BasicObject`; fix #4037

--- src/class.c
+++ src/class.c
@@ -1763,10 +1763,10 @@ mrb_class_path(mrb_state *mrb, struct RClass *c)
 MRB_API struct RClass*
 mrb_class_real(struct RClass* cl)
 {
-  if (cl == 0)
-    return NULL;
+  if (cl == 0) return NULL;
   while ((cl->tt == MRB_TT_SCLASS) || (cl->tt == MRB_TT_ICLASS)) {
     cl = cl->super;
+    if (cl == 0) return NULL;
   }
   return cl;
 }
