5fef38e4c77cb3700750fe0020063289a49242d6
yuri <yamayuri@gmail.com>
2018-07-10 15:44:25 +0900

    move mrb_irep_remove_lv to codegen.c in mruby-compiler

--- mrbgems/mruby-compiler/core/codegen.c
+++ mrbgems/mruby-compiler/core/codegen.c
@@ -3092,3 +3092,18 @@ mrb_generate_code(mrb_state *mrb, parser_state *p)
 {
   return generate_code(mrb, p, VAL);
 }
+
+void
+mrb_irep_remove_lv(mrb_state *mrb, mrb_irep *irep)
+{
+  int i;
+
+  if (irep->lv) {
+    mrb_free(mrb, irep->lv);
+    irep->lv = NULL;
+  }
+
+  for (i = 0; i < irep->rlen; ++i) {
+    mrb_irep_remove_lv(mrb, irep->reps[i]);
+  }
+}
