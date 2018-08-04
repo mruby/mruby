697b603e5a39845090165ec31244dddcdbb32703
Stefan Bilharz <stefan@sbsoftware.de>
2018-08-04 11:36:06 +0200

    Remove unused build configs

--- build_config.rb
+++ build_config.rb
@@ -85,46 +85,46 @@ MRuby::Build.new do |conf|
   # conf.enable_bintest
 end
 
-MRuby::Build.new('host-debug') do |conf|
-  # load specific toolchain settings
-
-  # Gets set by the VS command prompts.
-  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
-    toolchain :visualcpp
-  else
-    toolchain :gcc
-  end
-
-  enable_debug
-
-  # include the default GEMs
-  conf.gembox 'default'
-
-  # C compiler settings
-  conf.cc.defines = %w(MRB_ENABLE_DEBUG_HOOK)
-
-  # Generate mruby debugger command (require mruby-eval)
-  conf.gem :core => "mruby-bin-debugger"
-
-  # bintest
-  # conf.enable_bintest
-end
-
-MRuby::Build.new('test') do |conf|
-  # Gets set by the VS command prompts.
-  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
-    toolchain :visualcpp
-  else
-    toolchain :gcc
-  end
-
-  enable_debug
-  conf.enable_bintest
-  conf.enable_test
-
-  conf.gembox 'default'
-end
-
+#MRuby::Build.new('host-debug') do |conf|
+#  # load specific toolchain settings
+#
+#  # Gets set by the VS command prompts.
+#  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
+#    toolchain :visualcpp
+#  else
+#    toolchain :gcc
+#  end
+#
+#  enable_debug
+#
+#  # include the default GEMs
+#  conf.gembox 'default'
+#
+#  # C compiler settings
+#  conf.cc.defines = %w(MRB_ENABLE_DEBUG_HOOK)
+#
+#  # Generate mruby debugger command (require mruby-eval)
+#  conf.gem :core => "mruby-bin-debugger"
+#
+#  # bintest
+#  # conf.enable_bintest
+#end
+#
+#MRuby::Build.new('test') do |conf|
+#  # Gets set by the VS command prompts.
+#  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
+#    toolchain :visualcpp
+#  else
+#    toolchain :gcc
+#  end
+#
+#  enable_debug
+#  conf.enable_bintest
+#  conf.enable_test
+#
+#  conf.gembox 'default'
+#end
+#
 #MRuby::Build.new('bench') do |conf|
 #  # Gets set by the VS command prompts.
 #  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
