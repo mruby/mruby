b0dc163b5dadbdc8ef2241f3aa56a1eae428a22a
KOBAYASHI Shuji <shuujii@gmail.com>
2018-12-23 14:19:09 +0900

    Suppress a struct initializer warning
    Suppress a compiler (clang) warning bellow:
    
        src/vm.c:104:38: warning: suggest braces around initialization of
            subobject [-Wmissing-braces]
          const mrb_value mrb_value_zero = { 0 };
                                             ^
                                             {}

--- src/vm.c
+++ src/vm.c
@@ -101,7 +101,7 @@ static inline void
 stack_clear(mrb_value *from, size_t count)
 {
 #ifndef MRB_NAN_BOXING
-  const mrb_value mrb_value_zero = { 0 };
+  const mrb_value mrb_value_zero = { { 0 } };
 
   while (count-- > 0) {
     *from++ = mrb_value_zero;
