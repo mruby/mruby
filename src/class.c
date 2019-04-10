4776ac50ed39652e56a084475a5d79c1bbccc6c0
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2019-04-10 19:07:22 +0900

    Remove too aggressive `initialize` call in `mrb_instance_new`.

--- src/class.c
+++ src/class.c
@@ -1505,22 +1505,13 @@ mrb_instance_new(mrb_state *mrb, mrb_value cv)
   mrb_value *argv;
   mrb_int argc;
   mrb_sym init;
-  mrb_method_t m;
 
   mrb_get_args(mrb, "*&", &argv, &argc, &blk);
   obj = mrb_instance_alloc(mrb, cv);
   init = mrb_intern_lit(mrb, "initialize");
-  m = mrb_method_search(mrb, mrb_class(mrb, obj), init);
-  if (MRB_METHOD_CFUNC_P(m)) {
-    mrb_func_t f = MRB_METHOD_CFUNC(m);
-    if (f != mrb_bob_init) {
-      f(mrb, obj);
-    }
-  }
-  else {
+  if (!mrb_func_basic_p(mrb, obj, init, mrb_bob_init)) {
     mrb_funcall_with_block(mrb, obj, init, argc, argv, blk);
   }
-
   return obj;
 }
 
