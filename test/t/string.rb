4a8b88f7757f71d7d88b89764b533dd5ba59fd44
KOBAYASHI Shuji <shuujii@gmail.com>
2019-04-19 20:14:23 +0900

    Add type check (conversion) in `String#[]=`
    Before this patch:
    
      'a'[0] = 1        #=> 1
      'a'[:a] = '1'     #=> ArgumentError
      'a'[:a, 0] = '1'  #=> ArgumentError
      'a'[0, :a] = '1'  #=> ArgumentError
      'a'[0, 1] = 1     #=> 1
    
    After this patch / Ruby:
    
      'a'[0] = 1        #=> TypeError
      'a'[:a] = '1'     #=> TypeError
      'a'[:a, 0] = '1'  #=> TypeError
      'a'[0, :a] = '1'  #=> TypeError
      'a'[0, 1] = 1     #=> TypeError

--- test/t/string.rb
+++ test/t/string.rb
@@ -161,6 +161,9 @@ assert('String#[]=') do
    assert_equal 'aXc', e
   end
 
+  assert_raise(TypeError) { 'a'[0] = 1 }
+  assert_raise(TypeError) { 'a'[:a] = '1' }
+
   # length of args is 2
   a1 = 'abc'
   assert_raise(IndexError) do
@@ -197,6 +200,10 @@ assert('String#[]=') do
   assert_raise(IndexError) do
     b3['XX'] = 'Y'
   end
+
+  assert_raise(TypeError) { 'a'[:a, 0] = '1' }
+  assert_raise(TypeError) { 'a'[0, :a] = '1' }
+  assert_raise(TypeError) { 'a'[0, 1] = 1 }
 end
 
 assert('String#capitalize', '15.2.10.5.7') do
