ffa2fd129ce8d4ec7470cc772b1ffcd15f767d1a
KOBAYASHI Shuji <shuujii@gmail.com>
2019-05-18 18:39:18 +0900

    Move `tap` to `mruby-kernel-ext` from `mruby-object-ext`

--- mrbgems/mruby-kernel-ext/test/kernel.rb
+++ mrbgems/mruby-kernel-ext/test/kernel.rb
@@ -88,3 +88,19 @@ assert('Kernel#Hash') do
   assert_equal({:key => :value}, Hash(key: :value))
   assert_raise(TypeError) { Hash([1, 2, 3]) }
 end
+
+assert('Kernel#tap') do
+  ret = []
+  (1..10)                 .tap {|x| ret << "original: #{x.inspect}"}
+     .to_a                .tap {|x| ret << "array: #{x.inspect}"}
+     .select {|x| x%2==0} .tap {|x| ret << "evens: #{x.inspect}"}
+     .map { |x| x*x }     .tap {|x| ret << "squares: #{x.inspect}"}
+
+  assert_equal [
+    "original: 1..10",
+    "array: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]",
+    "evens: [2, 4, 6, 8, 10]",
+    "squares: [4, 16, 36, 64, 100]"
+  ], ret
+  assert_equal(:tap_ok, Class.new {def m; tap{return :tap_ok}; end}.new.m)
+end
