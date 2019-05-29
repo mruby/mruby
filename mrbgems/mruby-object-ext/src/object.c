03fce2e206128d6ae7fc5b7e3c66826446a5bad1
KOBAYASHI Shuji <shuujii@gmail.com>
2019-05-29 15:57:18 +0900

    `Kernel#itself` should not be a module function

--- mrbgems/mruby-object-ext/src/object.c
+++ mrbgems/mruby-object-ext/src/object.c
@@ -118,7 +118,8 @@ mrb_mruby_object_ext_gem_init(mrb_state* mrb)
   mrb_define_method(mrb, n, "to_f", nil_to_f,       MRB_ARGS_NONE());
 #endif
   mrb_define_method(mrb, n, "to_i", nil_to_i,       MRB_ARGS_NONE());
-  mrb_define_module_function(mrb, mrb->kernel_module, "itself", mrb_f_itself, MRB_ARGS_NONE());
+
+  mrb_define_method(mrb, mrb->kernel_module, "itself", mrb_f_itself, MRB_ARGS_NONE());
 
   mrb_define_method(mrb, mrb_class_get(mrb, "BasicObject"), "instance_exec", mrb_obj_instance_exec, MRB_ARGS_ANY() | MRB_ARGS_BLOCK());
 }
