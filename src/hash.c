8294ce9fd458a0a1acf8fcdcb6161b4a020866ad
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2019-07-04 23:11:57 +0900

    It was too early to check `key` for `undef`; ref #4534

--- src/hash.c
+++ src/hash.c
@@ -543,10 +543,10 @@ ht_copy(mrb_state *mrb, htable *t)
       mrb_value key = seg->e[i].key;
       mrb_value val = seg->e[i].val;
 
-      if (mrb_undef_p(key)) continue; /* skip deleted key */
       if ((seg->next == NULL) && (i >= t->last_len)) {
         return t2;
       }
+      if (mrb_undef_p(key)) continue; /* skip deleted key */
       ht_put(mrb, t2, key, val);
     }
     seg = seg->next;
