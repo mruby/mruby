682406a38425fd6389a67580e524b9c5c15c25fb
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2019-10-12 12:39:04 +0900

    Move `Array#difference` just after `Array#-`.

--- mrbgems/mruby-array-ext/mrblib/array.rb
+++ mrbgems/mruby-array-ext/mrblib/array.rb
@@ -90,6 +90,22 @@ class Array
 
   ##
   # call-seq:
+  #    ary.difference(other_ary1, other_ary2, ...)   -> new_ary
+  #
+  # Returns a new array that is a copy of the original array, removing all
+  # occurrences of any item that also appear in +other_ary+. The order is
+  # preserved from the original array.
+  #
+  def difference(*args)
+    ary = self
+    args.each do |x|
+      ary = ary - x
+    end
+    ary
+  end
+
+  ##
+  # call-seq:
   #    ary | other_ary     -> new_ary
   #
   # Set Union---Returns a new array by joining this array with
@@ -120,22 +136,6 @@ class Array
     args.each do |x|
       ary.concat(x)
       ary.uniq!
-    end
-    ary
-  end
-
-  ##
-  # call-seq:
-  #    ary.difference(other_ary1, other_ary2, ...)   -> new_ary
-  #
-  # Returns a new array that is a copy of the original array, removing all
-  # occurrences of any item that also appear in +other_ary+. The order is
-  # preserved from the original array.
-  #
-  def difference(*args)
-    ary = self
-    args.each do |x|
-      ary = ary - x
     end
     ary
   end
