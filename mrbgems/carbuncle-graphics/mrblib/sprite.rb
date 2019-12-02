727609ff79e6ceec9353f0d1d0c209724971df98
holywyvern <ramiro.rojo.cretta@gmail.com>
2019-12-02 02:18:39 -0300

    Add delegations to scale and pivot

--- mrbgems/carbuncle-graphics/mrblib/sprite.rb
+++ mrbgems/carbuncle-graphics/mrblib/sprite.rb
@@ -1,5 +1,7 @@
 class Carbuncle::Sprite
   delegate :x, :y, :x=, :y=, to: :position
+  delegate :x, :y, :x=, :y=, to: :scale, prefix: true
+  delegate :x, :y, :x=, :y=, to: :pivot, prefix: true
 
   def opacity
     color.alpha
