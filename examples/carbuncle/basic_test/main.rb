26de2597000e1859629b4d2a7bf571a1568ecbcf
holywyvern <ramiro.rojo.cretta@gmail.com>
2019-12-03 23:36:19 -0300

    Add some modifications to example

--- examples/carbuncle/basic_test/main.rb
+++ examples/carbuncle/basic_test/main.rb
@@ -1,11 +1,15 @@
 require 'scripts/player'
 
 class TestGame < Carbuncle::Game
-  attr_reader :player
+  attr_reader :player, :text
 
   def load
     @player = Player.new
     @player.position.set(player_x, player_y)
+    @text = Carbuncle::Text.new
+    @text.font = Carbuncle::Font.new('fonts/pixel-unicode.ttf', 64)
+    @text.value = 'Hëllo Wörld'
+    text.position.set((screen.width - text.width) / 2, (screen.height - text.height) / 2)
   end
 
   def player_x
@@ -18,9 +22,11 @@ class TestGame < Carbuncle::Game
 
   def update(dt)
     player.update(dt)
+    text.update(dt)
   end
 
   def draw
     player.draw
+    text.draw
   end
 end
