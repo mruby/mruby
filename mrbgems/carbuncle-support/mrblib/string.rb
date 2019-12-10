c3d8cac6a09192c5a4d092b45b6d9b5d24d12de5
holywyvern <ramiro.rojo.cretta@gmail.com>
2019-12-10 09:00:10 -0300

    adding spacing exchanger in underscore

--- mrbgems/carbuncle-support/mrblib/string.rb
+++ mrbgems/carbuncle-support/mrblib/string.rb
@@ -1,9 +1,10 @@
 class String
   def underscore
     gsub(/::/, '/')
-      .gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2')
-      .gsub(/([a-z\d])([A-Z])/,'\1_\2')
+      .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+      .gsub(/([a-z\d])([A-Z])/, '\1_\2')
       .tr('-', '_')
+      .tr(' ', '_')
       .downcase
   end
 end
