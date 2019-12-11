95343a18f74551a865ac39e0ac81c7342693c642
Ramiro Rojo <ramiro.rojo@wolox.com.ar>
2019-12-11 16:28:18 -0300

    make less calls to to_s

--- mrbgems/carbuncle-math/mrblib/matrix.rb
+++ mrbgems/carbuncle-math/mrblib/matrix.rb
@@ -91,10 +91,10 @@ module Carbuncle
     def inspect
       result = <<-HEREDOC
         Matrix:
-          #{self[0, 0]}, #{self[0, 1]}, #{self[0, 2]}, #{self[0, 3]},
-          #{self[1, 0]}, #{self[1, 1]}, #{self[1, 2]}, #{self[1, 3]},
-          #{self[2, 0]}, #{self[2, 1]}, #{self[2, 2]}, #{self[2, 3]},
-          #{self[3, 0]}, #{self[3, 1]}, #{self[3, 2]}, #{self[3, 3]}
+          #{rows[0].to_a.join(', ')},
+          #{rows[1].to_a.join(', ')},
+          #{rows[2].to_a.join(', ')},
+          #{rows[3].to_a.join(', ')},
       HEREDOC
       result.strip_heredoc
     end
