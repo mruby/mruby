86673ddf3b5807df1123655ce5204d41f6b77081
take-cheeze <takechi101010@gmail.com>
2019-12-13 23:40:09 +0900

    Set alloca size as string capa for debug purpose

--- src/gc.c
+++ src/gc.c
@@ -282,6 +282,7 @@ mrb_alloca(mrb_state *mrb, size_t size)
 {
   struct RString *s;
   s = (struct RString*)mrb_obj_alloc(mrb, MRB_TT_STRING, mrb->string_class);
+  s->as.heap.aux.capa = size;
   return s->as.heap.ptr = (char*)mrb_malloc(mrb, size);
 }
 
