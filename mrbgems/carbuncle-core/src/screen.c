8442d3afbe26ee255721691a4b3d2369c5c32e0a
holywyvern <ramiro.rojo.cretta@gmail.com>
2019-12-15 19:34:23 -0300

    Adding docs on screen as example

--- mrbgems/carbuncle-core/src/screen.c
+++ mrbgems/carbuncle-core/src/screen.c
@@ -37,9 +37,10 @@ mrb_cursor_new(mrb_state *mrb, mrb_value game)
 }
 
 /**
- * Creates a new screen object.
- * @param game [Carbuncle::Game] The game the screen corresponds to.
- * @returns self
+ * @overload initialize(game)
+ *   Creates a new screen object.
+ *   @param [Carbuncle::Game] game The game the screen corresponds to.
+ *   @return [self]
  */
 static mrb_value
 mrb_screen_initialize(mrb_state *mrb, mrb_value self)
@@ -58,8 +59,9 @@ mrb_screen_initialize(mrb_state *mrb, mrb_value self)
 }
 
 /**
- * The screen's width.
- * @returns [Integer]
+ * @overload width
+ *   The screen's width.
+ *   @return [Integer]
  */
 static mrb_value
 mrb_screen_get_width(mrb_state *mrb, mrb_value self)
@@ -69,8 +71,9 @@ mrb_screen_get_width(mrb_state *mrb, mrb_value self)
 }
 
 /**
- * The screen's height.
- * @returns [Integer]
+ * @overload height
+ *   The screen's title.
+ *   @return [Integer]
  */
 static mrb_value
 mrb_screen_get_height(mrb_state *mrb, mrb_value self)
@@ -79,24 +82,41 @@ mrb_screen_get_height(mrb_state *mrb, mrb_value self)
   return mrb_fixnum_value(screen->height);
 }
 
+/**
+ * @overload title
+ *   The screen's title.
+ *   @return [String]
+ */
 static mrb_value
 mrb_screen_get_title(mrb_state *mrb, mrb_value self)
 {
   return mrb_iv_get(mrb, self, TITLE_SYMBOL);
 }
 
+/**
+ * @return [Carbuncle::Game]
+ */
 static mrb_value
 mrb_screen_get_game(mrb_state *mrb, mrb_value self)
 {
   return mrb_iv_get(mrb, self, GAME_SYMBOL);
 }
 
+/**
+ * @return [Carbuncle::Screen::Cursor]
+ */
 static mrb_value
 mrb_screen_get_cursor(mrb_state *mrb, mrb_value self)
 {
   return mrb_iv_get(mrb, self, CURSOR_SYMBOL);
 }
 
+/**
+ * @overload width=(value)
+ *   Changes the screen's width.
+ *   @param [Integer] value The new screen width.
+ *   @return [Integer]
+ */
 static mrb_value
 mrb_screen_set_width(mrb_state *mrb, mrb_value self)
 {
@@ -115,6 +135,12 @@ mrb_screen_set_width(mrb_state *mrb, mrb_value self)
   return width;
 }
 
+/**
+ * @overload height=(value)
+ *   Changes the screen's height.
+ *   @param [Integer] value The new screen height.
+ *   @return [Integer]
+ */
 static mrb_value
 mrb_screen_set_height(mrb_state *mrb, mrb_value self)
 {
@@ -133,6 +159,12 @@ mrb_screen_set_height(mrb_state *mrb, mrb_value self)
   return height;
 }
 
+/**
+ * @overload title=(value)
+ *   Changes the screen's title.
+ *   @param [String] value The new screen title.
+ *   @return [String]
+ */
 static mrb_value
 mrb_screen_set_title(mrb_state *mrb, mrb_value self)
 {
@@ -149,6 +181,13 @@ mrb_screen_set_title(mrb_state *mrb, mrb_value self)
   return string_value;
 }
 
+/**
+ * @overload resize(width, height)
+ *   Changes the size of the screen.
+ *   @param [Integer] width the screen's new width.
+ *   @param [Integer] height the screen's new height.
+ *   @return [self]
+ */
 static mrb_value
 mrb_screen_resize(mrb_state *mrb, mrb_value self)
 {
@@ -162,6 +201,11 @@ mrb_screen_resize(mrb_state *mrb, mrb_value self)
   return self;
 }
 
+/**
+ * @overload initialize(game)
+ *   Assigns a new cursor to the corresponding game.
+ *   @param [Carbuncle::Game] game The game is attached to.
+ */
 static mrb_value
 mrb_cursor_initialize(mrb_state *mrb, mrb_value self)
 {
@@ -191,6 +235,19 @@ void
 mrb_init_carbuncle_screen(mrb_state *mrb)
 {
   struct RClass *carbuncle = mrb_carbuncle_get(mrb);
+  /**
+   * Represents the Screen, where all objects are drawn.
+   * @!attribute [rw] width
+   *   The screen's width.
+   * @!attribute [rw] height
+   *   The screen's height.
+   * @!attribute [rw] title
+   *   The screen's title.
+   * @!attribute [r] game
+   *   The screen's attached game.
+   * @!attribute [r] cursor
+   *   The screen's cursor.
+   */
   struct RClass *screen = mrb_define_class_under(mrb, carbuncle, "Screen", mrb->object_class);
   MRB_SET_INSTANCE_TT(screen, MRB_TT_DATA);
 
@@ -210,6 +267,10 @@ mrb_init_carbuncle_screen(mrb_state *mrb)
 
   mrb_define_method(mrb, screen, "resize", mrb_screen_resize, MRB_ARGS_REQ(2));
 
+  /**
+   * Handles the window's cursor.
+   * @attr_reader [true, false] Indicates if the cursor is visible or not.
+   */
   struct RClass *cursor = mrb_define_class_under(mrb, screen, "Cursor", mrb->object_class);
 
   mrb_define_method(mrb, cursor, "initialize", mrb_cursor_initialize, MRB_ARGS_REQ(1));
