1521aa90b34bb0c8974e828ec2e38ccf6b4eb299
holywyvern <ramiro.rojo.cretta@gmail.com>
2019-12-15 21:19:21 -0300

    Adding events as ruby code for better docs

---
+++ mrbgems/carbuncle-core/mrblib/game.rb
@@ -0,0 +1,53 @@
+module Carbuncle
+  class Game
+    class << self
+      # @!attribute [r] current_game
+      #   The current game, that's already running.
+      #   @return [Carbuncle::Game]
+      # @!method running?
+      #   Returns true if a game is running
+      #   @return [true, false]
+    end
+
+    # This method is called after the graphics context is created.
+    # This method should be overrided your main screen for your game.
+    # @example The game starts
+    #   class MyGame < Carbuncle::Game
+    #     def load
+    #       # The screen is loaded, so we can load our assets
+    #       SceneManager.set(TitleScene)
+    #     end
+    #   end
+    # @return [nil]
+    def load; end
+
+    # This method is called after an update, usually once per frame.
+    # If the application skips frames, this method won't be called.
+    # @param [Float] dt The time since the last update.
+    # @return [nil]
+    def update(dt); end
+
+    # This method is called once per frame. This is inside the drawing context,
+    # so you can only draw objects inside this function.
+    # @return [nil]
+    def draw; end
+
+    # This event is called before the game starts, even before the screen is initialized.
+    # If you require to load your assets, use Carbuncle::Game#load instead.
+    # @return [nil]
+    def before_run; end
+
+    # This event is called after the game starts, even after load.
+    # You can use this event to trigger your start, if you want to keep it separated from load.
+    # @return [nil]
+    def after_run; end
+
+    # This event is called before the game is closed, useful for cleanup.
+    # @return [nil]
+    def before_close; end
+
+    # This event is called after the game is closed and the screen is destroyed.
+    # @return [nil]
+    def after_close; end
+  end
+end
--- mrbgems/carbuncle-core/src/game.c
+++ mrbgems/carbuncle-core/src/game.c
@@ -193,42 +193,6 @@ mrb_game_closeQ(mrb_state *mrb, mrb_value self)
 }
 
 /**
- * @overload load
- *   This method is called after the graphics context is created.
- *   This method should be overrided your main screen for your game.
- *   @example The game starts
- *     class MyGame < Carbuncle::Game
- *       def load
- *         # The screen is loaded, so we can load our assets
- *         SceneManager.set(TitleScene)
- *       end
- *     end
- *   @return [nil]
- * @overload update(dt)
- *   This method is called after an update, usually once per frame.
- *   If the application skips frames, this method won't be called.
- *   @param [Float] dt The time since the last update.
- * @overload draw
- *   This method is called once per frame. This is inside the drawing context,
- *   so you can only draw objects inside this function.
- * @overload before_run
- *   This event is called before the game starts, even before the screen is initialized.
- *   If you require to load your assets, use Carbuncle::Game#load instead.
- * @overload after_run
- *   This event is called after the game starts, even after load.
- *   You can use this event to trigger your start, if you want to keep it separated from load.
- * @overload before_close
- *   This event is called before the game is closed, useful for cleanup.
- * @overload after_close
- *   This event is called after the game is closed and the screen is destroyed.
- */
-static mrb_value
-mrb_game_dummy(mrb_state *mrb, mrb_value self)
-{
-  return mrb_nil_value();
-}
-
-/**
  * Closes the game and destroys the game's window.
  * @return [self]
  */
@@ -284,16 +248,7 @@ mrb_init_carbuncle_game(mrb_state *mrb)
   mrb_define_method(mrb, game, "resize", mrb_game_resize, MRB_ARGS_REQ(2));
 
   /* Game lifecycle */
-  mrb_define_method(mrb, game, "load", mrb_game_dummy, MRB_ARGS_NONE());
-  mrb_define_method(mrb, game, "update", mrb_game_dummy, MRB_ARGS_REQ(1));
-  mrb_define_method(mrb, game, "draw", mrb_game_dummy, MRB_ARGS_NONE());
   mrb_define_method(mrb, game, "close", mrb_game_close, MRB_ARGS_NONE());
-
-  /* Lifecycle hooks */
-  mrb_define_method(mrb, game, "before_run", mrb_game_dummy, MRB_ARGS_NONE());
-  mrb_define_method(mrb, game, "after_run", mrb_game_dummy, MRB_ARGS_NONE());
-  mrb_define_method(mrb, game, "before_close", mrb_game_dummy, MRB_ARGS_NONE());
-  mrb_define_method(mrb, game, "after_close", mrb_game_dummy, MRB_ARGS_NONE());
 
   mrb_gv_set(mrb, CURRENT_GAME_SYMBOL, mrb_nil_value());
 }
