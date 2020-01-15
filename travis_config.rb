f7abda14370e6341dad180da3d04f8f2f4ff0cf0
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2020-01-15 23:39:19 +0900

    Remove `debug` target from `travis_config.rb`.

--- travis_config.rb
+++ travis_config.rb
@@ -1,17 +1,3 @@
-MRuby::Build.new('debug') do |conf|
-  toolchain :gcc
-  enable_debug
-
-  # include all core GEMs
-  conf.gembox 'full-core'
-  conf.cc.flags += %w(-Werror=declaration-after-statement)
-  conf.compilers.each do |c|
-    c.defines += %w(MRB_GC_STRESS MRB_GC_FIXED_ARENA MRB_METHOD_CACHE)
-  end
-
-  build_mrbc_exec
-end
-
 MRuby::Build.new('full-debug') do |conf|
   toolchain :gcc
   enable_debug
