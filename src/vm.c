ed0038fdf298750abeb9cf582f9576b3071e9411
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2020-04-21 15:43:35 +0900

    Simplified `mrb_obj_instance_eval`; ref #4973

--- src/vm.c
+++ src/vm.c
@@ -730,19 +730,8 @@ mrb_obj_instance_eval(mrb_state *mrb, mrb_value self)
   if (mrb_get_args(mrb, "|S&", &a, &b) == 1) {
     mrb_raise(mrb, E_NOTIMP_ERROR, "instance_eval with string not implemented");
   }
-  switch (mrb_type(self)) {
-  case MRB_TT_SYMBOL:
-  case MRB_TT_FIXNUM:
-#ifndef MRB_WITHOUT_FLOAT
-  case MRB_TT_FLOAT:
-#endif
-    c = 0;
-    break;
-  default:
-    cv = mrb_singleton_class(mrb, self);
-    c = mrb_class_ptr(cv);
-    break;
-  }
+  cv = mrb_singleton_class(mrb, self);
+  c = mrb_class_ptr(cv);
   return eval_under(mrb, self, b, c);
 }
 
