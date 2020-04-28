mruby/mruby

commit 992ba476a95136eaad5b9b208d4ca5a1ca31324d

Fix broken UTF-8 characters by `IO#getc`
Character (multi-byte UTF-8) is destroyed when character spanning
`IO::BUF_SIZE` (4096 bytes) exist.

- Prepare file:

  ```ruby
  File.open("sample", "wb") { |f| f << "●" * 1370 }
  ```

- Before patched:

  ```ruby
  File.open("sample") { |f| a = []; while ch = f.getc; a << ch; end; p a }
  # => ["●", "●", ..., "●", "\xe2", "\x97", "\x8f", "●", "●", "●", "●"]

- After patched:

  ```ruby
  File.open("sample") { |f| a = []; while ch = f.getc; a << ch; end; p a }
  # => ["●", "●", ..., "●", "●", "●", "●", "●", "●"]

mrbgems/mruby-io/mrblib/io.rb
@@ -170,8 +170,14 @@ class IO
   end
 
   def _read_buf
-    return @buf if @buf && @buf.bytesize > 0
-    @buf = sysread(BUF_SIZE)
+    return @buf if @buf && @buf.bytesize >= 4 # maximum UTF-8 character is 4 bytes
+    @buf ||= ""
+    begin
+      @buf += sysread(BUF_SIZE)
+    rescue EOFError => e
+      raise e if @buf.empty?
+    end
+    @buf
   end
 
   def ungetc(substr)

commit 3ce459f23a16a63973e1e0cfea299fb1ec108909

Fix `_read_buf` to be more efficient; fix #4982
The bug was introduced by #4712. The `getc' problem resurrected.
It should be addressed soon.

mrbgems/mruby-io/mrblib/io.rb
@@ -170,13 +170,8 @@ class IO
   end
 
   def _read_buf
-    return @buf if @buf && @buf.bytesize >= 4 # maximum UTF-8 character is 4 bytes
-    @buf ||= ""
-    begin
-      @buf += sysread(BUF_SIZE)
-    rescue EOFError => e
-      raise e if @buf.empty?
-    end
+    return @buf if @buf && @buf.bytesize > 0
+    sysread(BUF_SIZE, @buf)
   end
 
   def ungetc(substr)