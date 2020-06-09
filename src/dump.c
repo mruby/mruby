449317a251380c527b4cb296af95e51f4883d74c
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2020-06-09 15:16:19 +0900

    Removed alignment pragma from `-B` output from `mrbc`.
    We no longer need 4 bytes alignment after we moved to the byte oriented
    instructions.

--- src/dump.c
+++ src/dump.c
@@ -903,15 +903,8 @@ mrb_dump_irep_cfunc(mrb_state *mrb, const mrb_irep *irep, uint8_t flags, FILE *f
           "#ifdef __cplusplus\n"
           "extern const uint8_t %s[];\n"
           "#endif\n"
-          "const uint8_t\n"
-          "#if defined __GNUC__\n"
-          "__attribute__((aligned(%u)))\n"
-          "#elif defined _MSC_VER\n"
-          "__declspec(align(%u))\n"
-          "#endif\n"
-          "%s[] = {",
-          initname,
-          (uint16_t)MRB_DUMP_ALIGNMENT, (uint16_t)MRB_DUMP_ALIGNMENT, initname) < 0) {
+          "const uint8_t %s[] = {",
+          initname, initname) < 0) {
       mrb_free(mrb, bin);
       return MRB_DUMP_WRITE_FAULT;
     }
