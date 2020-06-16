015f06ed8ed613492630cab0d60660fc7eecbe28
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2020-06-16 12:08:29 +0900

    Should not mark red (i.e. ROM allocated) objects.

--- src/gc.c
+++ src/gc.c
@@ -772,6 +772,7 @@ mrb_gc_mark(mrb_state *mrb, struct RBasic *obj)
 {
   if (obj == 0) return;
   if (!is_white(obj)) return;
+  if (is_red(obj)) return;
   mrb_assert((obj)->tt != MRB_TT_FREE);
   add_gray_list(mrb, &mrb->gc, obj);
 }
