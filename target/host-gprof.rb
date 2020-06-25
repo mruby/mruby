fb5e8ab6d5fa9b7ca08c7b66ac8038940534db70
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2020-06-25 17:58:20 +0900

    Provide a new build target `host-gprof` that enables profiling.

---
+++ target/host-gprof.rb
@@ -0,0 +1,15 @@
+MRuby::Build.new do |conf|
+  # load specific toolchain settings
+  toolchain :gcc
+
+  # include the GEM box
+  conf.gembox 'full-core'
+
+  conf.cc.defines = %w(MRB_METHOD_CACHE)
+  conf.cc.flags << '-pg'
+  conf.linker.flags << '-pg'
+
+  # Turn on `enable_debug` for better debugging
+  enable_debug
+  conf.enable_test
+end
