mruby/mruby

commit 9dfe50f1d96178a9615b4fcdfe1ae2f5df71297f

Use `snprintf` instead of `memcpy` in `mrbc.c`.

mrbgems/mruby-bin-mrbc/tools/mrbc/mrbc.c
@@ -53,19 +53,13 @@ static char *
 get_outfilename(mrb_state *mrb, char *infile, const char *ext)
 {
   size_t infilelen;
-  size_t extlen;
+  size_t flen;
   char *outfile;
-  char *p;
 
   infilelen = strlen(infile);
-  extlen = strlen(ext);
-  outfile = (char*)mrb_malloc(mrb, infilelen + extlen + 1);
-  memcpy(outfile, infile, infilelen + 1);
-  if (*ext) {
-    if ((p = strrchr(outfile, '.')) == NULL)
-      p = outfile + infilelen;
-    memcpy(p, ext, extlen + 1);
-  }
+  flen = infilelen + strlen(ext) + 1;
+  outfile = (char*)mrb_malloc(mrb, flen);
+  snprintf(outfile, flen, "%s%s", infile, ext);
 
   return outfile;
 }

commit ac8360003ecfa1ec21295533c9213f17fd8e39bb

Fix the bug that `mrbc` generates `a.rb.mrb` instead of `a.mrb`.
The bug was introduced by 9dfe50f1d.

mrbgems/mruby-bin-mrbc/tools/mrbc/mrbc.c
@@ -52,14 +52,27 @@ usage(const char *name)
 static char *
 get_outfilename(mrb_state *mrb, char *infile, const char *ext)
 {
-  size_t infilelen;
-  size_t flen;
+  size_t ilen, flen, elen;
   char *outfile;
+  char *p = NULL;
 
-  infilelen = strlen(infile);
-  flen = infilelen + strlen(ext) + 1;
-  outfile = (char*)mrb_malloc(mrb, flen);
-  snprintf(outfile, flen, "%s%s", infile, ext);
+  ilen = strlen(infile);
+  flen = ilen;
+  if (*ext) {
+    elen = strlen(ext);
+    if ((p = strrchr(infile, '.'))) {
+      ilen = p - infile;
+    }
+    flen += elen;
+  }
+  else {
+    flen = ilen;
+  }
+  outfile = (char*)mrb_malloc(mrb, flen+1);
+  strncpy(outfile, infile, ilen+1);
+  if (p) {
+    strncpy(outfile+ilen, ext, elen+1);
+  }
 
   return outfile;
 }