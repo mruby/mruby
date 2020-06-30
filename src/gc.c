aa578c346db6058801a2b64781a50fab9ed1f74b
Hiroshi Mimaki <hiroshi.mimaki@gmail.com>
2020-06-30 18:33:47 +0900

    Revert "Free the original pointer if `realloc` failed."
    This reverts commit 9cdf439db52b66447b4e37c61179d54fad6c8f33.

--- src/gc.c
+++ src/gc.c
@@ -225,9 +225,14 @@ mrb_realloc(mrb_state *mrb, void *p, size_t len)
   p2 = mrb_realloc_simple(mrb, p, len);
   if (len == 0) return p2;
   if (p2 == NULL) {
-    mrb_free(mrb, p);
-    mrb->gc.out_of_memory = TRUE;
-    mrb_raise_nomemory(mrb);
+    if (mrb->gc.out_of_memory) {
+      mrb_raise_nomemory(mrb);
+      /* mrb_panic(mrb); */
+    }
+    else {
+      mrb->gc.out_of_memory = TRUE;
+      mrb_raise_nomemory(mrb);
+    }
   }
   else {
     mrb->gc.out_of_memory = FALSE;
