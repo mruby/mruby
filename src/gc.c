97319697c8f9f6ff27b32589947e1918e3015503
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2020-07-02 10:41:03 +0900

    Cancel 9cdf439
    Should not free the pointer in `realloc` since it can cause
    use-after-free problem.

--- src/gc.c
+++ src/gc.c
@@ -225,7 +225,6 @@ mrb_realloc(mrb_state *mrb, void *p, size_t len)
   p2 = mrb_realloc_simple(mrb, p, len);
   if (len == 0) return p2;
   if (p2 == NULL) {
-    mrb_free(mrb, p);
     mrb->gc.out_of_memory = TRUE;
     mrb_raise_nomemory(mrb);
   }
