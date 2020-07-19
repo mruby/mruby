00337543a5baf0e2cbedd65a697710a557b72041
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2020-07-19 09:11:16 +0900

    Should have updated the arg spec for `memsize_of`&`memsize_of_all`; #5040

--- mrbgems/mruby-objectspace/src/mruby_objectspace.c
+++ mrbgems/mruby-objectspace/src/mruby_objectspace.c
@@ -387,8 +387,8 @@ mrb_mruby_objectspace_gem_init(mrb_state *mrb)
   struct RClass *os = mrb_define_module(mrb, "ObjectSpace");
   mrb_define_class_method(mrb, os, "count_objects", os_count_objects, MRB_ARGS_OPT(1));
   mrb_define_class_method(mrb, os, "each_object", os_each_object, MRB_ARGS_OPT(1));
-  mrb_define_class_method(mrb, os, "memsize_of", os_memsize_of, MRB_ARGS_REQ(1)|MRB_ARGS_OPT(1));
-  mrb_define_class_method(mrb, os, "memsize_of_all", os_memsize_of_all, MRB_ARGS_REQ(1));
+  mrb_define_class_method(mrb, os, "memsize_of", os_memsize_of, MRB_ARGS_REQ(1));
+  mrb_define_class_method(mrb, os, "memsize_of_all", os_memsize_of_all, MRB_ARGS_OPT(1));
 }
 
 void
