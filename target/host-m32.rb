47d713b3346af52453c20d7eee88e84210e055b3
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2020-08-05 22:15:30 +0900

    Add `target/host-m32.rb` to compile mruby in 32bit mode.
    Tested only on Linux. You need to install 32bit relate libraries,
    e.g. libc6-dev-i386.

---
+++ target/host-m32.rb
@@ -0,0 +1,14 @@
+MRuby::Build.new do |conf|
+  # load specific toolchain settings
+  toolchain :gcc
+
+  # include the GEM box
+  conf.gembox 'full-core'
+
+  conf.cc.flags << '-m32'
+  conf.linker.flags << '-m32'
+
+  # Turn on `enable_debug` for better debugging
+  enable_debug
+  conf.enable_test
+end
