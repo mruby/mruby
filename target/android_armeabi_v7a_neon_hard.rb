1520c97e172484af7cbd0f51512cd3ac0025e228
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2020-08-16 17:56:50 +0900

    Add default `host` target for cross compiling.
    You don't have to define explicit `host` build target any more.

--- target/android_armeabi_v7a_neon_hard.rb
+++ target/android_armeabi_v7a_neon_hard.rb
@@ -1,18 +1,3 @@
-MRuby::Build.new do |conf|
-
-  # Gets set by the VS command prompts.
-  if ENV['VisualStudioVersion'] || ENV['VSINSTALLDIR']
-    toolchain :visualcpp
-  else
-    toolchain :gcc
-  end
-
-  enable_debug
-
-  # include the default GEMs
-  conf.gembox 'default'
-end
-
 # Requires Android NDK r13 or later.
 MRuby::CrossBuild.new('android-armeabi-v7a-neon-hard') do |conf|
   params = {
--- target/cross-32bit.rb
+++ target/cross-32bit.rb
@@ -1,13 +1,14 @@
 # Define cross build settings
-MRuby::CrossBuild.new('32bit') do |conf|
+MRuby::CrossBuild.new('cross-32bit') do |conf|
   toolchain :gcc
 
   conf.cc.flags << "-m32"
   conf.linker.flags << "-m32"
 
-  conf.build_mrbtest_lib_only
+  # conf.build_mrbtest_lib_only
 
-  conf.gem 'examples/mrbgems/c_and_ruby_extension_example'
+  conf.gem :core => "mruby-bin-mruby"
+  conf.gem "#{MRUBY_ROOT}/examples/mrbgems/c_and_ruby_extension_example"
 
   conf.test_runner.command = 'env'
 end
