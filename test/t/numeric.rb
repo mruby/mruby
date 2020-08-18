ed32b6fcdc0acc9e70f21a9df8e85d20ec662185
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2020-08-18 13:15:34 +0900

    Update tests for integer division.

--- test/t/numeric.rb
+++ test/t/numeric.rb
@@ -36,7 +36,7 @@ assert('Numeric#/', '15.2.8.3.4') do
   n = Class.new(Numeric){ def /(x); 15.1;end }.new
 
   assert_equal(2, 10/5)
-  assert_equal(0.0625, 1/16)
+  assert_equal(0, 1/16)
   assert_equal(15.1, n/10)
   assert_raise(TypeError){ 1/n }
   assert_raise(TypeError){ 1/nil }
