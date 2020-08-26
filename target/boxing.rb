befcbd5607a060e5e619cae7333512c8bdde32f6
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2020-08-27 08:42:07 +0900

    Add `MRB_NAN_BOXING` for `boxing` target.

--- target/boxing.rb
+++ target/boxing.rb
@@ -18,13 +18,12 @@ MRuby::Build.new('word_boxing') do |conf|
   conf.enable_test
 end
 
-# MRuby::Build.new('nan_boxing') do |conf|
-#   toolchain :gcc
+MRuby::Build.new('nan_boxing') do |conf|
+  toolchain :gcc
 
-#   conf.gembox 'default'
-#   conf.compilers.each do |c|
-#     c.defines += %w(MRB_NAN_BOXING)
-#   end
-#   conf.enable_test
-# end
-
+  conf.gembox 'default'
+  conf.compilers.each do |c|
+    c.defines += %w(MRB_NAN_BOXING)
+  end
+  conf.enable_test
+end
