0589749952ec6fc3dfb304eb66efa19e38ac9cb5
Hiroshi Mimaki <hiroshi.mimaki@gmail.com>
2020-10-16 10:43:03 +0900

    mruby 3.0.0-preview.

--- include/mruby/version.h
+++ include/mruby/version.h
@@ -27,7 +27,7 @@ MRB_BEGIN_DECL
 /*
  * The version of Ruby used by mruby.
  */
-#define MRUBY_RUBY_VERSION "2.0"
+#define MRUBY_RUBY_VERSION "3.0"
 
 /*
  * Ruby engine.
@@ -37,17 +37,17 @@ MRB_BEGIN_DECL
 /*
  * Major release version number.
  */
-#define MRUBY_RELEASE_MAJOR 2
+#define MRUBY_RELEASE_MAJOR 3
 
 /*
  * Minor release version number.
  */
-#define MRUBY_RELEASE_MINOR 1
+#define MRUBY_RELEASE_MINOR 0
 
 /*
  * Tiny release version number.
  */
-#define MRUBY_RELEASE_TEENY 2
+#define MRUBY_RELEASE_TEENY 0
 
 /*
  * The mruby version.
@@ -67,12 +67,12 @@ MRB_BEGIN_DECL
 /*
  * Release month.
  */
-#define MRUBY_RELEASE_MONTH 8
+#define MRUBY_RELEASE_MONTH 10
 
 /*
  * Release day.
  */
-#define MRUBY_RELEASE_DAY 6
+#define MRUBY_RELEASE_DAY 16
 
 /*
  * Release date as a string.
