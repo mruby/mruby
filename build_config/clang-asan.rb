bad622391441d1ad9e6f97d72be49b1ef2933099
KOBAYASHI Shuji <shuujii@gmail.com>
2020-11-13 16:30:18 +0900

    Use `enable_sanitizer` to simplify `build_config/clang-asan.rb`

--- build_config/clang-asan.rb
+++ build_config/clang-asan.rb
@@ -1,21 +1,11 @@
 MRuby::Build.new do |conf|
-  toolchain :clang
+  conf.toolchain :clang
   # include the GEM box
   conf.gembox 'default'
 
-  # C compiler settings
-  conf.cc do |cc|
-    cc.flags << "-fsanitize=address,undefined"
-    cc.include_paths = ["#{root}/include"]
-  end
-
-  # Linker settings
-  conf.linker do |linker|
-    linker.flags << "-fsanitize=address,undefined"
-  end
-
   # Turn on `enable_debug` for better debugging
-  enable_debug
+  conf.enable_sanitizer "address,undefined"
+  conf.enable_debug
   conf.enable_bintest
   conf.enable_test
 end
