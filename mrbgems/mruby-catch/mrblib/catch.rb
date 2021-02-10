c7809ca025b3b81dd6fb695f27bd5d0dfa3174af
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2021-02-10 20:09:00 +0900

    Refactor `mruby-catch`; ref #5328
    - Move `#catch` definition to `mruby-catch.c` to avoid tweaking
    - Remove `#__preserve_catch_method`
    - Implement whole `#throw` method in C

--- mrbgems/mruby-catch/mrblib/catch.rb
+++ mrbgems/mruby-catch/mrblib/catch.rb
@@ -6,17 +6,3 @@ class UncaughtThrowError < ArgumentError
     super("uncaught throw #{tag.inspect}")
   end
 end
-
-module Kernel
-  def catch(tag=Object.new, &block)
-    # A double closure is required to make the nested `catch` distinguishable
-    # and because `break` goes back to `proc->upper`.
-    -> { -> { block.call(tag) }.call }.call
-  end
-  def throw(tag, val=nil)
-    __throw(tag, val)
-    raise UncaughtThrowError.new(tag, val)
-  end
-
-  __preserve_catch_method
-end
--- mrbgems/mruby-catch/src/mruby-catch.c
+++ mrbgems/mruby-catch/src/mruby-catch.c
@@ -3,7 +3,63 @@
 #include <mruby/variable.h>
 #include <mruby/error.h>
 #include <mruby/proc.h>
+#include <mruby/opcode.h>
 #include <mruby/presym.h>
+
+
+static const mrb_sym catch_syms_3[1] = {MRB_SYM(call),};
+static const mrb_code catch_iseq_3[18] = {
+  OP_ENTER,    0x00, 0x00, 0x00,
+  OP_GETUPVAR, 0x02, 0x02, 0x01,
+  OP_GETUPVAR, 0x03, 0x01, 0x01,
+  OP_SEND,     0x02, 0x00, 0x01,
+  OP_RETURN,   0x02,};
+static const mrb_irep catch_irep_3 = {
+  2,5,0,
+  MRB_IREP_STATIC,catch_iseq_3,
+  NULL,catch_syms_3,NULL,
+  NULL,
+  NULL,
+  18,0,1,0,0
+};
+static const mrb_irep *catch_reps_2[1] = {
+  &catch_irep_3,
+};
+static const mrb_code catch_iseq_2[13] = {
+  OP_ENTER,    0x00, 0x00, 0x00,
+  OP_LAMBDA,   0x02, 0x00,
+  OP_SEND,     0x02, 0x00, 0x00,
+  OP_RETURN,   0x02,};
+static const mrb_irep catch_irep_2 = {
+  2,4,0,
+  MRB_IREP_STATIC,catch_iseq_2,
+  NULL,catch_syms_3,catch_reps_2,
+  NULL,
+  NULL,
+  13,0,1,1,0
+};
+static const mrb_irep *catch_reps_1[1] = {
+  &catch_irep_2,
+};
+static const mrb_sym catch_syms_1[3] = {MRB_SYM(Object), MRB_SYM(new), MRB_SYM(call),};
+static const mrb_code catch_iseq_1[29] = {
+  OP_ENTER,    0x00, 0x20, 0x01,
+  OP_JMP,      0x00, 0x03,
+  OP_JMP,      0x00, 0x0a,
+  OP_GETCONST, 0x03, 0x00,
+  OP_SEND,     0x03, 0x01, 0x00,
+  OP_MOVE,     0x01, 0x03,
+  OP_LAMBDA,   0x03, 0x00,
+  OP_SEND,     0x03, 0x02, 0x00,
+  OP_RETURN,   0x03,};
+static const mrb_irep catch_irep = {
+  3,5,0,
+  MRB_IREP_STATIC,catch_iseq_1,
+  NULL,catch_syms_1,catch_reps_1,
+  NULL,
+  NULL,
+  29,0,3,1,0
+};
 
 #define ID_PRESERVED_CATCH MRB_SYM(__preserved_catch_proc)
 
@@ -31,7 +87,9 @@ static mrb_value
 mrb_f_throw(mrb_state *mrb, mrb_value self)
 {
   mrb_value tag, obj;
-  mrb_get_args(mrb, "oo", &tag, &obj);
+  if (mrb_get_args(mrb, "o|o", &tag, &obj) == 1) {
+    obj = mrb_nil_value();
+  }
 
   const mrb_callinfo *ci = find_catcher(mrb, tag);
   if (ci) {
@@ -40,28 +98,26 @@ mrb_f_throw(mrb_state *mrb, mrb_value self)
     mrb_break_proc_set(b, ci[2].proc); /* Back to the closure in `catch` method */
     mrb_exc_raise(mrb, mrb_obj_value(b));
   }
-
-  return mrb_nil_value();
-}
-
-static mrb_value
-mrb_s_preserve_catch(mrb_state *mrb, mrb_value self)
-{
-  mrb_method_t m = mrb_method_search(mrb, mrb->kernel_module, MRB_SYM(catch));
-  mrb_assert(!MRB_METHOD_UNDEF_P(m));
-  mrb_assert(!MRB_METHOD_CFUNC_P(m));
-  mrb_obj_iv_set(mrb, (struct RObject *)mrb->kernel_module, ID_PRESERVED_CATCH, mrb_obj_value(MRB_METHOD_PROC(m)));
-
-  mrb_remove_method(mrb, mrb_class(mrb, mrb_obj_value(mrb->kernel_module)), MRB_SYM(__preserve_catch_method));
-
+  else {
+    mrb_value argv[2] = {tag, obj};
+    mrb_exc_raise(mrb, mrb_obj_new(mrb, mrb_exc_get_id(mrb, MRB_ERROR_SYM(UncaughtThrowError)), 2, argv));
+  }
+  /* not reached */
   return mrb_nil_value();
 }
 
 void
 mrb_mruby_catch_gem_init(mrb_state *mrb)
 {
-  mrb_define_method(mrb, mrb->kernel_module, "__throw", mrb_f_throw, MRB_ARGS_REQ(2));
-  mrb_define_class_method(mrb, mrb->kernel_module, "__preserve_catch_method", mrb_s_preserve_catch, MRB_ARGS_NONE());
+  struct RProc *p;
+  mrb_method_t m;
+
+  p = mrb_proc_new(mrb, &catch_irep);
+  MRB_METHOD_FROM_PROC(m, p);
+  mrb_define_method_raw(mrb, mrb->kernel_module, MRB_SYM(catch), m);
+  mrb_obj_iv_set(mrb, (struct RObject *)mrb->kernel_module, ID_PRESERVED_CATCH, mrb_obj_value(p));
+
+  mrb_define_method(mrb, mrb->kernel_module, "throw", mrb_f_throw, MRB_ARGS_ARG(1,1));
 }
 
 void
