d64c7330bc847ac2b6ae078e837c6dcdd3605381
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2021-02-22 13:38:31 +0900

    print.c: fix a compilation errors with `MRB_NO_STDIO`; fix #5361

--- src/print.c
+++ src/print.c
@@ -47,17 +47,7 @@ mrb_p(mrb_state *mrb, mrb_value obj)
     printstr(mrb_inspect(mrb, obj), stdout);
   }
 }
-#else
-void
-mrb_core_init_printabort(void)
-{
-}
 
-MRB_API void
-mrb_p(mrb_state *mrb, mrb_value obj)
-{
-}
-#endif
 
 MRB_API void
 mrb_print_error(mrb_state *mrb)
@@ -76,3 +66,30 @@ mrb_show_copyright(mrb_state *mrb)
 {
   printstr(mrb_const_get(mrb, mrb_obj_value(mrb->object_class), MRB_SYM(MRUBY_COPYRIGHT)), stdout);
 }
+
+#else
+void
+mrb_core_init_printabort(void)
+{
+}
+
+MRB_API void
+mrb_p(mrb_state *mrb, mrb_value obj)
+{
+}
+
+MRB_API void
+mrb_print_error(mrb_state *mrb)
+{
+}
+
+MRB_API void
+mrb_show_version(mrb_state *mrb)
+{
+}
+
+MRB_API void
+mrb_show_copyright(mrb_state *mrb)
+{
+}
+#endif
