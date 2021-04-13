7fe6f3976eca19a86fe533deb4c60b31cd80a236
Ryan Lopopolo <rjl@hyperbo.la>
2021-04-13 07:57:09 -0700

    Fix incorrect prototype on declaration of mrb_protect_atexit
    `state.c` makes a prototype declaration for the private
    `mrb_protect_atexit` which is defined in `error.c`. `error.c` defines
    this function with a void return type, but `state.c` defines the
    prototype with an `int` return type.
    
    This mismatch prevents mruby from compiling on stricter compilers like
    emscripten.

--- src/state.c
+++ src/state.c
@@ -183,7 +183,7 @@ mrb_free_context(mrb_state *mrb, struct mrb_context *c)
   mrb_free(mrb, c);
 }
 
-int mrb_protect_atexit(mrb_state *mrb);
+void mrb_protect_atexit(mrb_state *mrb);
 
   MRB_API void
 mrb_close(mrb_state *mrb)
