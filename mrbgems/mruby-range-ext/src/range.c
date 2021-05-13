2bed750eb0efe8da35e1b03d55cc1f47a4f960ff
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2021-05-13 15:35:34 +0900

    range.c: avoid use of `mrb_fixnum_p`.
    `mrb_int` may not fit in `fixnum` (inline integer).

--- mrbgems/mruby-range-ext/src/range.c
+++ mrbgems/mruby-range-ext/src/range.c
@@ -81,7 +81,7 @@ range_size(mrb_state *mrb, mrb_value range)
 
   beg = RANGE_BEG(r);
   end = RANGE_END(r);
-  if ((mrb_fixnum_p(beg) || mrb_float_p(beg)) && mrb_nil_p(end)) {
+  if ((mrb_integer_p(beg) || mrb_float_p(beg)) && mrb_nil_p(end)) {
     return mrb_float_value(mrb, INFINITY);
   }
 
@@ -136,7 +136,7 @@ range_size(mrb_state *mrb, mrb_value range)
 
   beg = RANGE_BEG(r);
   end = RANGE_END(r);
-  if (mrb_fixnum_p(beg) && mrb_nil_p(end)) {
+  if (mrb_integer_p(beg) && mrb_nil_p(end)) {
     return mrb_nil_value();
   }
 
@@ -147,7 +147,7 @@ range_size(mrb_state *mrb, mrb_value range)
     mrb_int b = mrb_integer(end);
     mrb_int c = b - a + excl;
 
-    return mrb_fixnum_value(c);
+    return mrb_int_value(mrb, c);
   }
   return mrb_nil_value();
 }
