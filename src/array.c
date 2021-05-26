9a72869043e0377ec549a325a074e592f82ef5ed
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2021-05-27 08:13:49 +0900

    array.c: check for negative shift size.

--- src/array.c
+++ src/array.c
@@ -591,6 +591,7 @@ mrb_ary_shift_m(mrb_state *mrb, mrb_value self)
   };
   ary_modify_check(mrb, a);
   if (len == 0 || n == 0) return mrb_ary_new(mrb);
+  if (n < 0) mrb_raise(mrb, E_ARGUMENT_ERROR, "negative array shift");
   if (n > len) n = len;
   val = mrb_ary_new_from_values(mrb, n, ARY_PTR(a));
   if (ARY_SHARED_P(a)) {
