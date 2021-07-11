0712a5406ac28b62b8d0709fa0efd85547b59e41
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2021-07-12 08:17:43 +0900

    load.c: call `mrb_top_run()` from `mrb_load_proc()`; fix #5504
    So that the function behave consistently with `mrb_load_proc()`.

--- src/load.c
+++ src/load.c
@@ -683,7 +683,7 @@ mrb_load_irep_buf(mrb_state *mrb, const void *buf, size_t bufsize)
 MRB_API mrb_value
 mrb_load_proc(mrb_state *mrb, const struct RProc *proc)
 {
-  return mrb_vm_run(mrb, proc, mrb_top_self(mrb), 0);
+  return mrb_top_run(mrb, proc, mrb_top_self(mrb), 0);
 }
 
 #ifndef MRB_NO_STDIO
