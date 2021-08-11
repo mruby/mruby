80893811c0e56d7cc61b100d546a44ceab2e43a4
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2021-08-12 08:10:09 +0900

    class.c: `const_missing` do not have to be in the backtrace; ref #5528

--- src/class.c
+++ src/class.c
@@ -2460,6 +2460,7 @@ mrb_mod_const_missing(mrb_state *mrb, mrb_value mod)
   mrb_sym sym;
 
   mrb_get_args(mrb, "n", &sym);
+  mrb->c->ci->mid = 0;
 
   if (mrb_class_real(mrb_class_ptr(mod)) != mrb->object_class) {
     mrb_name_error(mrb, sym, "uninitialized constant %v::%n", mod, sym);
