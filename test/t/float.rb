8ca40da3fe49d01b31ef4258ff578c9dd61f09b4
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2021-08-20 17:12:09 +0900

    test/float.rb: avoid `1.0e16` in tests without precision specified.

--- test/t/float.rb
+++ test/t/float.rb
@@ -277,8 +277,6 @@ assert('Float#to_s') do
   assert_equal("-1.0e-10", -0.0000000001.to_s)
   assert_equal("1.0e+20", 1e20.to_s)
   assert_equal("-1.0e+20", -1e20.to_s)
-  assert_equal("1.0e+16", 10000000000000000.0.to_s)
-  assert_equal("-1.0e+16", -10000000000000000.0.to_s)
   assert_equal("100000.0", 100000.0.to_s)
   assert_equal("-100000.0", -100000.0.to_s)
   if uses_float
