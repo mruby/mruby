d2a904c8848238f2e144332db61d1f613dd956c3
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2021-12-08 16:43:02 +0900

    vm.c (check_method_noarg): the value at `kidx` may not be a hash.

--- src/vm.c
+++ src/vm.c
@@ -544,8 +544,11 @@ static void
 check_method_noarg(mrb_state *mrb, const mrb_callinfo *ci)
 {
   int argc = ci->n == CALL_MAXARGS ? RARRAY_LEN(ci->stack[1]) : ci->n;
-  if (ci->nk != 0 && !mrb_hash_empty_p(mrb, ci->stack[ci->n == CALL_MAXARGS ? 2 : ci->n + 1])) {
-    argc++;
+  if (ci->nk > 0) {
+    mrb_value kdict = ci->stack[mrb_ci_kidx(ci)];
+    if (!(mrb_hash_p(kdict) && mrb_hash_empty_p(mrb, kdict))) {
+      argc++;
+    }
   }
   if (argc > 0) {
     mrb_argnum_error(mrb, argc, 0, 0);
