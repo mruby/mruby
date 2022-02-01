d9bf7982e9c3e2bdcda66bf2acc90180448f90ee
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2022-02-01 22:36:42 +0900

    class.c: set `MRB_FL_CLASS_IS_INHERITED` flag for bootstrapped classes.

--- src/class.c
+++ src/class.c
@@ -1376,6 +1376,7 @@ boot_defclass(mrb_state *mrb, struct RClass *super)
   if (super) {
     c->super = super;
     mrb_field_write_barrier(mrb, (struct RBasic*)c, (struct RBasic*)super);
+    c->flags |= MRB_FL_CLASS_IS_INHERITED;
   }
   else {
     c->super = mrb->object_class;
