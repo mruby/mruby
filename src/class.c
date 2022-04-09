b1d0296a937fe278239bdfac840a3fd0e93b3ee9
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2022-04-09 09:25:35 +0900

    class.c: clear method cache after `remove_method`.

--- src/class.c
+++ src/class.c
@@ -2361,7 +2361,10 @@ mrb_remove_method(mrb_state *mrb, struct RClass *c, mrb_sym mid)
   MRB_CLASS_ORIGIN(c);
   h = c->mt;
 
-  if (h && mt_del(mrb, h, mid)) return;
+  if (h && mt_del(mrb, h, mid)) {
+    mrb_mc_clear_by_class(mrb, c);
+    return;
+  }
   mrb_name_error(mrb, mid, "method '%n' not defined in %C", mid, c);
 }
 
