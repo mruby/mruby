37c2f080f09390105956edb2565627f29281e24e
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2022-07-12 14:48:36 +0900

    numeric.c (int_to_s): should not bypass base range check.

--- src/numeric.c
+++ src/numeric.c
@@ -1805,11 +1805,6 @@ int_to_s(mrb_state *mrb, mrb_value self)
   mrb_int base = 10;
 
   mrb_get_args(mrb, "|i", &base);
-#ifdef MRB_USE_BIGINT
-  if (mrb_bigint_p(self)) {
-    return mrb_bint_to_s(mrb, self, base);
-  }
-#endif
   return mrb_integer_to_str(mrb, self, base);
 }
 
