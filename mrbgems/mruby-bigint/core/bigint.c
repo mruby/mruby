26ce85bd5de515906e01fdeeac774316628ad599
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2022-07-26 00:13:15 +0900

    bigint.c (ucmp): fixed error with zero length zero value.
    Big integers have two kind of zero values:
    
    - digits in `p` array are all zeros
    - length `sn` is zero
    
    The old code crashed with the latter zero.

--- mrbgems/mruby-bigint/core/bigint.c
+++ mrbgems/mruby-bigint/core/bigint.c
@@ -186,6 +186,7 @@ ucmp(mpz_t *y, mpz_t *x)
 {
   if (y->sz < x->sz) return -1;
   if (y->sz > x->sz) return 1;
+  if (x->sz == 0) return 0;
   for (size_t i=x->sz-1;; i--) {
     mp_limb a = y->p[i];
     mp_limb b = x->p[i];
