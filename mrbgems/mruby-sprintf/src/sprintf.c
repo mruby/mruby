6bcbfed8bb955a4412e0ca79da0244f97076ba18
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2022-11-11 08:08:07 +0900

    mruby-sprintf/sprintf.c: check integer overflow before casting.

--- mrbgems/mruby-sprintf/src/sprintf.c
+++ mrbgems/mruby-sprintf/src/sprintf.c
@@ -259,7 +259,7 @@ get_num(mrb_state *mrb, const char *p, const char *end, int *valp)
 {
   char *e;
   mrb_int n;
-  if (!mrb_read_int(p, end, &e, &n)) {
+  if (!mrb_read_int(p, end, &e, &n) || INT_MAX < n) {
     return NULL;
   }
   *valp = (int)n;
