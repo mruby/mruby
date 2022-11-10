9fcfd01f205fdd092be0932ac0b7a6a4eb29d2b3
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2022-11-10 22:21:12 +0900

    mruby-pack/pack.c (read_tmpl): check integer overflow before casting.

--- mrbgems/mruby-pack/src/pack.c
+++ mrbgems/mruby-pack/src/pack.c
@@ -1306,7 +1306,7 @@ alias:
     if (ISDIGIT(ch)) {
       char *e;
       mrb_int n;
-      if (!mrb_read_int(tptr+tmpl->idx, tptr+tlen, &e, &n)) {
+      if (!mrb_read_int(tptr+tmpl->idx, tptr+tlen, &e, &n) || INT_MAX < n) {
         mrb_raise(mrb, E_RUNTIME_ERROR, "too big template length");
       }
       count = (int)n;
