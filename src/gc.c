375d22b41260d9229485dcdedffda638d016720c
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2022-12-06 15:26:24 +0900

    gc.c (clear_all_old): avoid calling GC twice in the function

--- src/gc.c
+++ src/gc.c
@@ -1287,12 +1287,13 @@ clear_all_old(mrb_state *mrb, mrb_gc *gc)
     /* finish the half baked GC */
     incremental_gc_finish(mrb, gc);
   }
-
-  /* Sweep the dead objects, then reset all the live objects
-   * (including all the old objects, of course) to white. */
-  gc->generational = FALSE;
-  prepare_incremental_sweep(mrb, gc);
-  incremental_gc_finish(mrb, gc);
+  else {
+    /* Sweep the dead objects, then reset all the live objects
+     * (including all the old objects, of course) to white. */
+    gc->generational = FALSE;
+    prepare_incremental_sweep(mrb, gc);
+    incremental_gc_finish(mrb, gc);
+  }
   gc->generational = origin_mode;
 
   /* The gray objects have already been painted as white */
