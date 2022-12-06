38098ddf2aac2928220b41dfb15459bf13ce230b
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2022-12-06 15:45:39 +0900

    class.c (prepare_singleton_class): remove unnecessary write barrier
    Since sc (singleton class) is a newly created object so it's not needed
    to call write barrier.

--- src/class.c
+++ src/class.c
@@ -392,7 +392,6 @@ prepare_singleton_class(mrb_state *mrb, struct RBasic *o)
   }
   o->c = sc;
   mrb_field_write_barrier(mrb, (struct RBasic*)o, (struct RBasic*)sc);
-  mrb_field_write_barrier(mrb, (struct RBasic*)sc, (struct RBasic*)o);
   mrb_obj_iv_set(mrb, (struct RObject*)sc, MRB_SYM(__attached__), mrb_obj_value(o));
   sc->flags |= o->flags & MRB_FL_OBJ_IS_FROZEN;
 }
