d0659b089f3d9eb8b7969bc2c006032d10076d8b
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2022-12-30 09:29:37 +0900

    mruby-io/io.c: remove wrong ISO section comment

--- mrbgems/mruby-io/src/io.c
+++ mrbgems/mruby-io/src/io.c
@@ -1636,7 +1636,7 @@ mrb_init_io(mrb_state *mrb)
   mrb_define_method(mrb, io, "close_on_exec=", io_set_close_on_exec, MRB_ARGS_REQ(1));
   mrb_define_method(mrb, io, "close_on_exec?", io_close_on_exec_p,   MRB_ARGS_NONE());
   mrb_define_method(mrb, io, "closed?",    io_closed,     MRB_ARGS_NONE());   /* 15.2.20.5.2 */
-  mrb_define_method(mrb, io, "pid",        io_pid,        MRB_ARGS_NONE());   /* 15.2.20.5.2 */
+  mrb_define_method(mrb, io, "pid",        io_pid,        MRB_ARGS_NONE());
   mrb_define_method(mrb, io, "fileno",     io_fileno,     MRB_ARGS_NONE());
   mrb_define_method(mrb, io, "write",      io_write,      MRB_ARGS_ANY());    /* 15.2.20.5.20 */
   mrb_define_method(mrb, io, "pread",      io_pread,      MRB_ARGS_ANY());    /* ruby 2.5 feature */
