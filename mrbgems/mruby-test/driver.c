d9145b8621cf6e4023c41a36617d672bf428daec
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2023-01-11 10:43:11 +0900

    mruby-test/drive.c: add string type check

--- mrbgems/mruby-test/driver.c
+++ mrbgems/mruby-test/driver.c
@@ -273,6 +273,7 @@ mrb_t_pass_result(mrb_state *mrb_dst, mrb_state *mrb_src)
     mrb_value res_dst = mrb_gv_get(mrb_dst, mrb_intern_lit(mrb_dst, "$asserts"));
     for (i = 0; i < RARRAY_LEN(res_src); i++) {
       mrb_value val_src = RARRAY_PTR(res_src)[i];
+      mrb_ensure_string_type(mrb_dst, val_src);
       mrb_ary_push(mrb_dst, res_dst, mrb_str_new(mrb_dst, RSTRING_PTR(val_src), RSTRING_LEN(val_src)));
     }
   }
