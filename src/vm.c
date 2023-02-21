43922a5bf9ece3bc6d1421a35265a6256ebc89f0
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2023-02-21 21:59:42 +0900

    vm.c (mrb_gc_arena_shrink): use local variable

--- src/vm.c
+++ src/vm.c
@@ -65,7 +65,7 @@ mrb_gc_arena_shrink(mrb_state *mrb, int idx)
   mrb_gc *gc = &mrb->gc;
   int capa = gc->arena_capa;
 
-  mrb->gc.arena_idx = idx;
+  gc->arena_idx = idx;
   if (idx < capa / 4) {
     capa >>= 2;
     if (capa < MRB_GC_ARENA_SIZE) {
