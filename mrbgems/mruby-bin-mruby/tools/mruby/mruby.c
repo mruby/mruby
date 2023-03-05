38d5ed44e1f0003a077acdf62208076fe4dc0528
dearblue <dearblue@users.osdn.me>
2023-03-05 17:45:47 +0900

    Avoid exposure for `REnv` objects
    The `REnv` object is difficult to deal with, and it would be ideal if the user did not have to manipulate it directly.
    In some previous situations, it was necessary to call `mrb_env_unshare()`, a non-API function, after `mrb_load_string()` or similar.
    
    With this patch, it is no longer necessary for users to use `mrb_env_unshare()` directly, as it is now handled internally simply by using the `mrb_vm_ci_env_clear()` function.
    Also, `mrb_vm_ci_env_set()` is demoted from the `MRB_API` function for the same reason.
    
    ref. commit 1ab3da6f080f987d7b763b651557af006c8aef1d

--- mrbgems/mruby-bin-mruby/tools/mruby/mruby.c
+++ mrbgems/mruby-bin-mruby/tools/mruby/mruby.c
@@ -326,7 +326,6 @@ main(int argc, char **argv)
 
     /* Load libraries */
     for (i = 0; i < args.libc; i++) {
-      struct REnv *e;
       FILE *lfp = fopen(args.libv[i], "rb");
       if (lfp == NULL) {
         fprintf(stderr, "%s: Cannot open library file: %s\n", *argv, args.libv[i]);
@@ -342,9 +341,7 @@ main(int argc, char **argv)
         v = mrb_load_detect_file_cxt(mrb, lfp, c);
       }
       fclose(lfp);
-      e = mrb_vm_ci_env(mrb->c->cibase);
-      mrb_vm_ci_env_set(mrb->c->cibase, NULL);
-      mrb_env_unshare(mrb, e, FALSE);
+      mrb_vm_ci_env_clear(mrb, mrb->c->cibase);
       mrbc_cleanup_local_variables(mrb, c);
     }
 
