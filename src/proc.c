b885d7387d0c9967b845f9097eeaf729edac50cf
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2023-03-27 11:33:13 +0900

    proc.c: no need to define Proc.lambda
    Since Kernel is an ancestor of Kernel module itself recursively.

--- src/proc.c
+++ src/proc.c
@@ -470,6 +470,5 @@ mrb_init_proc(mrb_state *mrb)
   mrb_define_method_raw(mrb, mrb->proc_class, MRB_SYM(call), m);
   mrb_define_method_raw(mrb, mrb->proc_class, MRB_OPSYM(aref), m);
 
-  mrb_define_class_method(mrb, mrb->kernel_module, "lambda", proc_lambda, MRB_ARGS_NONE()|MRB_ARGS_BLOCK()); /* 15.3.1.2.6  */
   mrb_define_method(mrb, mrb->kernel_module,       "lambda", proc_lambda, MRB_ARGS_NONE()|MRB_ARGS_BLOCK()); /* 15.3.1.3.27 */
 }
