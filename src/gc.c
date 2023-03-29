f1568af0d279f0784c8ef4c22f8d8c7de136ac75
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2023-03-29 22:55:45 +0900

    gc.c: fix GC regressions
    The past fix was wrong.

--- src/gc.c
+++ src/gc.c
@@ -1349,11 +1349,12 @@ mrb_field_write_barrier(mrb_state *mrb, struct RBasic *obj, struct RBasic *value
   mrb_assert(gc->state == MRB_GC_STATE_MARK || (!is_dead(gc, value) && !is_dead(gc, obj)));
   mrb_assert(is_generational(gc) || gc->state != MRB_GC_STATE_ROOT);
 
-  if (gc->state == MRB_GC_STATE_SWEEP) {
-    paint_partial_white(gc, obj); /* for never write barriers */
+  if (is_generational(gc) || gc->state == MRB_GC_STATE_MARK) {
+    add_gray_list(mrb, gc, value);
   }
   else {
-    add_gray_list(mrb, gc, value);
+    mrb_assert(gc->state == MRB_GC_STATE_SWEEP);
+    paint_partial_white(gc, obj); /* for never write barriers */
   }
 }
 
