mruby/mruby

commit 375d22b41260d9229485dcdedffda638d016720c

gc.c (clear_all_old): avoid calling GC twice in the function

src/gc.c
@@ -1287,12 +1287,13 @@ clear_all_old(mrb_state *mrb, mrb_gc *gc)
     /* finish the half baked GC */
     incremental_gc_finish(mrb, gc);
   }
-
-  /* Sweep the dead objects, then reset all the live objects
-   * (including all the old objects, of course) to white. */
-  gc->generational = FALSE;
-  prepare_incremental_sweep(mrb, gc);
-  incremental_gc_finish(mrb, gc);
+  else {
+    /* Sweep the dead objects, then reset all the live objects
+     * (including all the old objects, of course) to white. */
+    gc->generational = FALSE;
+    prepare_incremental_sweep(mrb, gc);
+    incremental_gc_finish(mrb, gc);
+  }
   gc->generational = origin_mode;
 
   /* The gray objects have already been painted as white */

commit b47c8b738ae3e400ec02269fae9857222218e9d5

gc.c (clear_all_old): fix a generational GC bug
This issue was caused by 375d22b that introduced a serious mark
leakage in the generational GC mode.

src/gc.c
@@ -1240,14 +1240,12 @@ clear_all_old(mrb_state *mrb, mrb_gc *gc)
     /* finish the half baked GC */
     incremental_gc_finish(mrb, gc);
   }
-  else {
-    /* Sweep the dead objects, then reset all the live objects
-     * (including all the old objects, of course) to white. */
-    gc->generational = FALSE;
-    prepare_incremental_sweep(mrb, gc);
-    incremental_gc_finish(mrb, gc);
-    gc->generational = TRUE;
-  }
+  /* Sweep the dead objects, then reset all the live objects
+   * (including all the old objects, of course) to white. */
+  gc->generational = FALSE;
+  prepare_incremental_sweep(mrb, gc);
+  incremental_gc_finish(mrb, gc);
+  gc->generational = TRUE;
   /* The gray objects have already been painted as white */
   gc->atomic_gray_list = gc->gray_list = NULL;
 }