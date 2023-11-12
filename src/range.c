a94005b74075b10e1e1d218661973266a39869fa
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2023-11-12 22:27:46 +0900

    range.c: use presym to initialize Range class

--- src/range.c
+++ src/range.c
@@ -504,7 +504,7 @@ mrb_init_range(mrb_state *mrb)
 {
   struct RClass *r;
 
-  r = mrb_define_class(mrb, "Range", mrb->object_class);                                /* 15.2.14 */
+  r = mrb_define_class_id(mrb, MRB_SYM(Range), mrb->object_class);                                /* 15.2.14 */
   mrb->range_class = r;
   MRB_SET_INSTANCE_TT(r, MRB_TT_RANGE);
 
