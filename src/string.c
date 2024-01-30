219cfd63e151360efe664728e3b5dbcf6e102d44
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2024-01-31 07:42:52 +0900

    string.c (mrb_memsearch_ss): update integer prefixes (LLU -> ULL)

--- src/string.c
+++ src/string.c
@@ -580,20 +580,20 @@ mrb_memsearch_ss(const unsigned char *xs, long m, const unsigned char *ys, long 
 {
 #ifdef MRB_64BIT
 #define bitint uint64_t
-#define MASK1 0x0101010101010101llu
-#define MASK2 0x7f7f7f7f7f7f7f7fllu
-#define MASK3 0x8080808080808080llu
+#define MASK1 0x0101010101010101ull
+#define MASK2 0x7f7f7f7f7f7f7f7full
+#define MASK3 0x8080808080808080ull
 #else
 #define bitint uint32_t
-#define MASK1 0x01010101lu
-#define MASK2 0x7f7f7f7flu
-#define MASK3 0x80808080lu
+#define MASK1 0x01010101ul
+#define MASK2 0x7f7f7f7ful
+#define MASK3 0x80808080ul
 #endif
 #if defined(MRB_ENDIAN_BIG)
 #ifdef MRB_64BIT
-#define MASK4 0x8000000000000000llu
+#define MASK4 0x8000000000000000ull
 #else
-#define MASK4 0x80000000
+#define MASK4 0x80000000ul
 #endif
 #else
 #define MASK4 0x80
