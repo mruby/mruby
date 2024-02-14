32279e4128527bab4c961854b9cce727a060abea
mimaki <hiroshi.mimaki@gmail.com>
2024-02-14 13:25:11 +0900

    Update version and release date. (mruby 3.3.0 (2024-02-14))

--- doc/guides/debugger.md
+++ doc/guides/debugger.md
@@ -38,7 +38,7 @@ To confirm mrdb was installed properly, run mrdb with the `--version` option:
 
 ```bash
 $ mrdb --version
-mruby 3.2.0 (2023-02-24)
+mruby 3.3.0 (2024-02-14)
 ```
 
 ## 2.2 Basic Operation
--- include/mruby/version.h
+++ include/mruby/version.h
@@ -57,7 +57,7 @@ MRB_BEGIN_DECL
 /*
  * Patch level string. (optional)
  */
-#define MRUBY_PATCHLEVEL_STR "RC2"
+#define MRUBY_PATCHLEVEL_STR ""
 
 #ifndef MRUBY_PATCHLEVEL_STR
 # if MRUBY_PATCHLEVEL < 0
@@ -85,12 +85,12 @@ MRB_BEGIN_DECL
 /*
  * Release month.
  */
-#define MRUBY_RELEASE_MONTH 1
+#define MRUBY_RELEASE_MONTH 2
 
 /*
  * Release day.
  */
-#define MRUBY_RELEASE_DAY 4
+#define MRUBY_RELEASE_DAY 14
 
 /*
  * Release date as a string.
