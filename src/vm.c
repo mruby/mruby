mruby/mruby

commit 42308c42b548c30e54663e70902fc3eca7b22ea8

Fix CI misalignment between `Fiber#resume` and `Fiber.yield`
When a fiber switched by `Fiber.yield` is resumed by `Fiber#resume` by C, it is necessary to pop CI with `fiber_switch()`.
Previously, CI misalignment caused inconsistencies, including crashes, on the next `Fiber#resume`.

src/vm.c
@@ -1839,6 +1839,7 @@ RETRY_TRY_BLOCK:
           }
           recv = MRB_METHOD_FUNC(m)(mrb, recv);
         }
+        mrb_assert(mrb->c->ci > mrb->c->cibase);
         mrb_gc_arena_shrink(mrb, ai);
         if (mrb->exc) goto L_RAISE;
         ci = mrb->c->ci;

mrbgems/mruby-fiber/src/fiber.c
@@ -272,6 +272,7 @@ fiber_switch(mrb_state *mrb, mrb_value self, mrb_int len, const mrb_value *a, mr
   else {
     value = fiber_result(mrb, a, len);
     if (vmexec) {
+      if (c->ci > c->cibase) c->ci--; /* pop dummy callinfo */
       c->ci[1].stack[0] = value;
     }
   }
@@ -412,7 +413,6 @@ mrb_fiber_yield(mrb_state *mrb, mrb_int len, const mrb_value *a)
   if (c->vmexec) {
     c->vmexec = FALSE;
     mrb->c->ci->cci = CINFO_RESUMED;
-    c->ci--;                    /* pop callinfo for yield */
   }
   MARK_CONTEXT_MODIFY(mrb->c);
   return fiber_result(mrb, a, len);

commit 9616971c705b4ad2422bc4bb9de834f8bcb9c83b

Fix wrong assertion in `OP_SENDB`.
Assertions were failing on exit if started with `mrb_fiber_resume()`.

The bug that caused it was introduced by commit 42308c42b548c30e54663e70902fc3eca7b22ea8 (#6106).
The bug was moved by commit 990e18ad59e8428bd644559d852a3842994b7e94.

src/vm.c
@@ -1912,7 +1912,6 @@ RETRY_TRY_BLOCK:
       }
 
       /* cfunc epilogue */
-      mrb_assert(mrb->c->ci > mrb->c->cibase);
       mrb_gc_arena_shrink(mrb, ai);
       if (mrb->exc) goto L_RAISE;
       ci = mrb->c->ci;
@@ -1929,6 +1928,7 @@ RETRY_TRY_BLOCK:
           syms = irep->syms;
         }
       }
+      mrb_assert(ci > mrb->c->cibase);
       ci->stack[0] = recv;
       /* pop stackpos */
       ci = cipop(mrb);