d94916bf15a7b9a1b5d2cc14ebeec0e45cf4fce7
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2024-06-13 15:33:04 +0900

    mruby-kernel-ext: use presym for initialization

--- mrbgems/mruby-kernel-ext/src/kernel.c
+++ mrbgems/mruby-kernel-ext/src/kernel.c
@@ -270,17 +270,17 @@ mrb_mruby_kernel_ext_gem_init(mrb_state *mrb)
 {
   struct RClass *krn = mrb->kernel_module;
 
-  mrb_define_module_function(mrb, krn, "fail", mrb_f_raise, MRB_ARGS_OPT(2));
-  mrb_define_module_function(mrb, krn, "caller", mrb_f_caller, MRB_ARGS_OPT(2));
-  mrb_define_method(mrb, krn, "__method__", mrb_f_method, MRB_ARGS_NONE());
-  mrb_define_method(mrb, krn, "__callee__", mrb_f_callee, MRB_ARGS_NONE());
-  mrb_define_module_function(mrb, krn, "Integer", mrb_f_integer, MRB_ARGS_ARG(1,1));
+  mrb_define_module_function_id(mrb, krn, MRB_SYM(fail), mrb_f_raise, MRB_ARGS_OPT(2));
+  mrb_define_module_function_id(mrb, krn, MRB_SYM(caller), mrb_f_caller, MRB_ARGS_OPT(2));
+  mrb_define_method_id(mrb, krn, MRB_SYM(__method__), mrb_f_method, MRB_ARGS_NONE());
+  mrb_define_method_id(mrb, krn, MRB_SYM(__callee__), mrb_f_callee, MRB_ARGS_NONE());
+  mrb_define_module_function_id(mrb, krn, MRB_SYM(Integer), mrb_f_integer, MRB_ARGS_ARG(1,1));
 #ifndef MRB_NO_FLOAT
-  mrb_define_module_function(mrb, krn, "Float", mrb_f_float, MRB_ARGS_REQ(1));
+  mrb_define_module_function_id(mrb, krn, MRB_SYM(Float), mrb_f_float, MRB_ARGS_REQ(1));
 #endif
-  mrb_define_module_function(mrb, krn, "String", mrb_f_string, MRB_ARGS_REQ(1));
-  mrb_define_module_function(mrb, krn, "Array", mrb_f_array, MRB_ARGS_REQ(1));
-  mrb_define_module_function(mrb, krn, "Hash", mrb_f_hash, MRB_ARGS_REQ(1));
+  mrb_define_module_function_id(mrb, krn, MRB_SYM(String), mrb_f_string, MRB_ARGS_REQ(1));
+  mrb_define_module_function_id(mrb, krn, MRB_SYM(Array), mrb_f_array, MRB_ARGS_REQ(1));
+  mrb_define_module_function_id(mrb, krn, MRB_SYM(Hash), mrb_f_hash, MRB_ARGS_REQ(1));
 }
 
 void
