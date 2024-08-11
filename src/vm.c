22eb7e87af2ff2a43a851ee96881fa59dd2937af
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2024-08-11 23:45:40 +0900

    vm.c (uvenv): move declaration to initialization

--- src/vm.c
+++ src/vm.c
@@ -222,13 +222,12 @@ static inline struct REnv*
 uvenv(mrb_state *mrb, mrb_int up)
 {
   const struct RProc *proc = mrb->c->ci->proc;
-  struct REnv *e;
 
   while (up--) {
     proc = proc->upper;
     if (!proc) return NULL;
   }
-  e = MRB_PROC_ENV(proc);
+  struct REnv *e = MRB_PROC_ENV(proc);
   if (e) return e;              /* proc has enclosed env */
   return NULL;
 }
