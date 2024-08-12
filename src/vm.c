c853dc409014253942b8d509e67cbd2b93edc610
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2024-08-12 22:56:29 +0900

    vm.c (funcall): move va_list declaration right before va_start()

--- src/vm.c
+++ src/vm.c
@@ -526,13 +526,13 @@ MRB_API mrb_value
 mrb_funcall(mrb_state *mrb, mrb_value self, const char *name, mrb_int argc, ...)
 {
   mrb_value argv[MRB_FUNCALL_ARGC_MAX];
-  va_list ap;
   mrb_sym mid = mrb_intern_cstr(mrb, name);
 
   if (argc > MRB_FUNCALL_ARGC_MAX) {
     mrb_raise(mrb, E_ARGUMENT_ERROR, "Too long arguments. (limit=" MRB_STRINGIZE(MRB_FUNCALL_ARGC_MAX) ")");
   }
 
+  va_list ap;
   va_start(ap, argc);
   for (mrb_int i = 0; i < argc; i++) {
     argv[i] = va_arg(ap, mrb_value);
@@ -545,12 +545,12 @@ MRB_API mrb_value
 mrb_funcall_id(mrb_state *mrb, mrb_value self, mrb_sym mid, mrb_int argc, ...)
 {
   mrb_value argv[MRB_FUNCALL_ARGC_MAX];
-  va_list ap;
 
   if (argc > MRB_FUNCALL_ARGC_MAX) {
     mrb_raise(mrb, E_ARGUMENT_ERROR, "Too long arguments. (limit=" MRB_STRINGIZE(MRB_FUNCALL_ARGC_MAX) ")");
   }
 
+  va_list ap;
   va_start(ap, argc);
   for (mrb_int i = 0; i < argc; i++) {
     argv[i] = va_arg(ap, mrb_value);
