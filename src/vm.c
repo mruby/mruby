98440e095f6f3e43bd9b851307338a03c7119433
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2024-08-19 23:39:03 +0900

    vm.c (mrb_ci_nregs): unify declaration and initialization

--- src/vm.c
+++ src/vm.c
@@ -590,11 +590,9 @@ mrb_ci_bidx(mrb_callinfo *ci)
 mrb_int
 mrb_ci_nregs(mrb_callinfo *ci)
 {
-  const struct RProc *p;
-
   if (!ci) return 4;
   mrb_int nregs = ci_bidx(ci) + 1; /* self + args + kargs + blk */
-  p = ci->proc;
+  const struct RProc *p = ci->proc;
   if (p && !MRB_PROC_CFUNC_P(p) && p->body.irep && p->body.irep->nregs > nregs) {
     return p->body.irep->nregs;
   }
