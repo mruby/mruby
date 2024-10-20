1f5ba7e01d01450f29d78092856d925652917010
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2024-10-20 19:30:45 +0900

    vm.c (OP_STRCAT): mrb_str_concat may call VM recursively
    And recursive calls may reallocate call-info stack.

--- src/vm.c
+++ src/vm.c
@@ -2774,6 +2774,7 @@ RETRY_TRY_BLOCK:
     CASE(OP_STRCAT, B) {
       mrb_assert(mrb_string_p(regs[a]));
       mrb_str_concat(mrb, regs[a], regs[a+1]);
+      ci = mrb->c->ci;
       NEXT;
     }
 
