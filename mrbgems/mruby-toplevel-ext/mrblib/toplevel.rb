1e4a3f3e5c4208c40fee6dfc172c96f8fa4f1e9c
Yukihiro "Matz" Matsumoto <matz@ruby.or.jp>
2025-02-01 14:56:20 +0900

    mruby-toplevel-ext: implement toplevel public/private/protected

--- mrbgems/mruby-toplevel-ext/mrblib/toplevel.rb
+++ mrbgems/mruby-toplevel-ext/mrblib/toplevel.rb
@@ -2,9 +2,22 @@ def self.include(*modules)
   Object.include(*modules)
 end
 
-def self.private(*methods)
-end
-def self.protected(*methods)
-end
-def self.public(*methods)
+class <<self
+  private
+
+  def private(*methods)
+    Object.instance_eval do
+      private(*methods)
+    end
+  end
+  def protected(*methods)
+    Object.instance_eval do
+      protected(*methods)
+    end
+  end
+  def public(*methods)
+    Object.instance_eval do
+      public(*methods)
+    end
+  end
 end
