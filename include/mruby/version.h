2a435caf794208d1fbdd0c1fa4a4b58422b0af4e
mimaki <hiroshi.mimaki@gmail.com>
2025-03-17 16:31:36 +0900

    Update version to 3.4.0RC2.

--- include/mruby/version.h
+++ include/mruby/version.h
@@ -57,7 +57,7 @@ MRB_BEGIN_DECL
 /*
  * Patch level string. (optional)
  */
-#define MRUBY_PATCHLEVEL_STR "RC"
+#define MRUBY_PATCHLEVEL_STR "RC2"
 
 #ifndef MRUBY_PATCHLEVEL_STR
 # if MRUBY_PATCHLEVEL < 0
@@ -90,7 +90,7 @@ MRB_BEGIN_DECL
 /*
  * Release day.
  */
-#define MRUBY_RELEASE_DAY 10
+#define MRUBY_RELEASE_DAY 17
 
 /*
  * Release date as a string.
